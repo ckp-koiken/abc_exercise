f6889078d9962f09b6adb26e05de71b79173f386
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-02 13:58:49 +0900

    add interpretaion

--- abc401/B_Unauthorized.cpp
+++ abc401/B_Unauthorized.cpp
@@ -5,21 +5,26 @@ int main() {
   int N;
   cin >> N;
 
+  // ログイン状態を示すフラグ
   bool logged_in = false;
+  // 認証エラーを返す回数
   int unauthorized = 0;
 
   for (int i = 0; i < N; i++) {
     string s;
     cin >> s;
 
+    // ログインしたならばフラグをtrueに
     if (s == "login") {
       logged_in = true;
     }
 
+    // ログアウトしたならばフラグをfalseに
     if (s == "logout") {
       logged_in = false;
     }
 
+    // ログインしてない状態で非公開ページにアクセスしたときエラーの回数を増やす
     if (s == "private" && !logged_in) {
       unauthorized++;
     }
