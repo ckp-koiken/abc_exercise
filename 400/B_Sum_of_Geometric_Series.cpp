67e01cdef3461ccb43336356822ad9619c186768
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-04 19:22:07 +0900

    fix answer

--- 400/B_Sum_of_Geometric_Series.cpp
+++ 400/B_Sum_of_Geometric_Series.cpp
@@ -7,10 +7,11 @@ int main() {
   cin >> N >> M;
   int max = 1e9;
 
-  int64_t X = 1;
+  // 合計値（初期値0）
+  int64_t X = 0;
+  // 各累乗項（0乗から始まるので初期値0）
   int64_t num = 1;
-  for (int i = 1; i <= M; i++) {
-    num *= N;
+  for (int i = 0; i <= M; i++) {
     X += num;
     // Xの数は64bit整数の範囲で表現できないほど大きくなる
     // 最大値に達した時にinfを表示して処理を打ち切る
@@ -18,6 +19,7 @@ int main() {
       cout << "inf" << endl;
       return 0;
     }
+    num *= N;
   }
   
   cout << X << endl;
