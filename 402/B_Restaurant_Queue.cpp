39cbd30584158570857f79bb0a4644c4e1c9baf6
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-07 19:01:41 +0900

    add interpretation

--- 402/B_Restaurant_Queue.cpp
+++ 402/B_Restaurant_Queue.cpp
@@ -5,17 +5,22 @@ int main() {
   int Q;
   cin >> Q;
 
+  // キュー・待ち行列用にqueueを用意する
   queue<int> que;
 
   for (int i = 0; i < Q; i++) {
+    // クエリの先頭を読む
     int t;
     cin >> t;
     if (t == 1) {
+      // クエリが1ならその隣のメニューをキューに追加(.push(x))
       int x;
       cin >> x;
-      que.push(x);
+      que.push(x);  
     } else {
+      // クエリが2ならキューから順番にメニューを表示(.front())
       cout << que.front() << endl;
+      // キューの先頭要素を削除(.pop())
       que.pop();
     }
   }
