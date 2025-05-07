a48a3b3ca3b9dc1fe431b06c27a48ec4207ef25f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-07 18:22:54 +0900

    add 402 & solve A prob

---
+++ 402/A_CBC.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  for (int i = 0; i < S.size(); i++) {
+    if (S.at(i) < 97) {
+      cout << S.at(i);
+    }
+  }  
+
+  cout << endl;
+  return 0;
+}
