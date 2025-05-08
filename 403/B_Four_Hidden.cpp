2eb5f33c9f48a873073e82ad5aafcf364600b006
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-08 20:03:04 +0900

    add another answer

--- 403/B_Four_Hidden.cpp
+++ 403/B_Four_Hidden.cpp
@@ -39,3 +39,32 @@ int main() {
   cout << "No" << endl;
   return 0;
 }
+
+// another answer
+/*
+int main() {
+  string t, u;
+  cin >> t >> u;
+  int n = t.size(), m = u.size();
+
+  // uがtの連続部分文字列でありえる場所を探索する
+  // 連続部分文字列でありえる場合の和はn - m + 1
+  // tの文字列からuの文字が連続して含まれうる場所を探す
+  for (int l = 0; l < n - m + 1; l++) {
+    bool ok = true; // 連続部分文字列でありうるかどうかを判定
+    // 連続部分文字列の先頭からuの文字数分探索
+    for (int i = 0; i < m; i++) {
+      // 探索している場所が?でない、かつ、uの該当文字でない場合はfalse
+      if (t.at(l + i) != '?' && t.at(l + i) != u.at(i)) {
+        ok = false;
+      }
+    }
+    if (ok) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
+*/
