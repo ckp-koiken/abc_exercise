95b1e1379362ca7e43d53f589a74f5f2419e23ce
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-11 12:35:09 +0900

    add B prob

---
+++ 397/B_Ticket_Gate_Log.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  // TODO: add interpretation
+  int ans = 0;
+  char target = 'i';
+  for (char c : S) {
+    if (c == target) {
+      target = target == 'i' ? 'o' : 'i';
+    } else {
+      ans++;
+    }
+  }
+  if (target == 'o') {
+    ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
