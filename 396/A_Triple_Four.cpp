49b8107b3f056ea701b71f5c7a70f76d2d93e982
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-12 08:44:44 +0900

    add 396 & solve A prob

---
+++ 396/A_Triple_Four.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+
+  int check = 0;
+  for (int i = 0; i < N - 2; i++) {
+    if (A.at(i) == A.at(i + 1) && A.at(i) == A.at(i + 2)){
+      check++;
+    } 
+  }
+
+  if (check > 0) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+
+  return 0;
+}
