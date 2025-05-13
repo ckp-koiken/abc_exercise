97de7d9214264174f450c735304595735b55996e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-13 15:34:36 +0900

    add interpretation

--- 395/B_Make_Target.cpp
+++ 395/B_Make_Target.cpp
@@ -4,17 +4,28 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
+
+  // 要素nのベクトルを用意する（'.'で初期化）
+  // 文字'.'をn個配置した文字列をn個配置した2次元配列を作る
+  // std::string(...)でcharから文字列作成
   vector<string> s(n, string(n, '.'));
+
+  // iをl、jをrとしてn回ループ
+  // 0始まりのインデックスとするため
   for (int l = 0; l < n; l++) {
-    int r = n - 1 - l;
+    int r = n - 1 - l;  // j = n + 1 - i を1減らしたもの
+
+    // l > rならなにもしない
     if (l > r) {
       continue;
     }
 
-    char c = '#';
-    if (l % 2) {
-      c = '.';
-    }
+    // 挿入文字をcとする
+    // lが偶数なら'.'にする
+    char c = l % 2 ? '.' : '#';
+
+    // iとjをlからrまでループしながらcを代入
+    // 条件は問題文の指示通り
     for (int i = l; i <= r; i++) {
       for (int j = l; j <= r; j++) {
         s.at(i).at(j) = c;
@@ -22,8 +33,10 @@ int main() {
     }
   }
 
+  // 表示
   for (int i = 0; i < n; i++) {
     cout << s.at(i) << endl;
   }
+
   return 0;
 }
