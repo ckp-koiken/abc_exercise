ccfb0d8d97185ec749c30534999b841c3b60741c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-15 08:54:06 +0900

    solve A prob

---
+++ 393/A_Poisonous_Oyster.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S1, S2;
+  cin >> S1 >> S2;
+
+  if (S1 == S2 && S1 == "fine") {
+    cout << 4 << endl;
+  } else if (S1 == S2 && S1 == "sick") {
+    cout << 1 << endl;
+  } else if (S1 != S2 && S1 == "sick") {
+    cout << 2 << endl;
+  } else if (S1 != S2 && S2 == "sick") {
+    cout << 3 << endl;
+  } 
+  return 0;
+}
