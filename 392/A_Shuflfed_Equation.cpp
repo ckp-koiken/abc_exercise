406d0d43ba4f79a3db286cca32b9e8be8a4bd18b
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-17 20:33:23 +0900

    add anotehr answer

--- 392/A_Shuflfed_Equation.cpp
+++ 392/A_Shuflfed_Equation.cpp
@@ -13,3 +13,15 @@ int main() {
 
   return 0;
 }
+
+// another answer
+  // 交換法則をつかう
+  // vector<int> a(3);
+  // cin >> a.at(0) >> a.at(1) >> a.at(2);
+  // sort(a.begin(), a.end());
+  // if (a.at(0) * a.at(1) == a.at(2)) {
+  //   cout << "Yes" << endl;
+  // } else {
+  //   cout << "No" << endl;
+  // }
+
