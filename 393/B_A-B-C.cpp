cf7b557b0f8461dcc3fb235c5776fc3e77a4e58b
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-18 17:59:20 +0900

    solve B prob

---
+++ 393/B_A-B-C.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  int length = S.size();
+  int ans = 0;
+
+  for (int i = 0; i < length; i++) {
+    for (int j = i + 1; j < length; j++) {
+      for (int k = j + 1; k < length; k++) {
+        if (S.at(i) == 'A' && S.at(j) == 'B' && S.at(k) == 'C' &&
+            j - i == k - j) {
+          ans++;
+        }
+      }
+    }
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
