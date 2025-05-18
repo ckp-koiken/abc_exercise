125fc037e7092be8cfb87312467bb608ef1b9f9e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-18 17:33:10 +0900

    add interpretation

--- 405/B_Not_All.cpp
+++ 405/B_Not_All.cpp
@@ -9,15 +9,24 @@ int main() {
     cin >> A.at(i);
   }
 
+  // 0からN回まで末尾を削除する
+  // それぞれの回で1~Nまでの数が全部揃っているかを調べる
   for (int num = 0; num < N + 1; num++) {
+    // 要素が重複しないコンテナsetを使う
+    // 配列Aをsetにまとめて要素の種類数を求める
     set<int> st;
     for (int x : A) {
       st.insert(x);
     }
+    // set<int> st(A.begin(), A.end());　でも書ける
+
+    // setのサイズがMでないならば処理を終了
     if (st.size() != M) {
       cout << num << endl;
       return 0;
     }
+    // setのサイズがMなら1~Nまでの数が全部揃っているので
+    // 末尾をpop_back()で削除
     A.pop_back();
   }
   return 0;
