19def2feb09f265251ca68007965d74589a56875
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-20 15:26:45 +0900

    add 390 & solve A prob

---
+++ 390/A_12435.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> a(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> a.at(i);
+  }
+
+  for (int i = 0; i < 4; i++) {
+    vector<int> b(5);
+    int check = 0;
+
+    b = a;
+    int tmp;
+    tmp = b.at(i + 1);
+    b.at(i + 1) = b.at(i);
+    b.at(i) = tmp;
+
+    for (int j = 0; j < 5; j++) {
+      if (b.at(j) == (j + 1)) {
+        check++;
+      }
+    }
+    if (check == 5) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+
+  cout << "No" << endl;
+  return 0;
+}
