e4093a488a175a71045a67f7383fd1871fa37119
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-20 22:31:06 +0900

    add interpretation

--- 390/B_Geometric_Sequence.cpp
+++ 390/B_Geometric_Sequence.cpp
@@ -9,9 +9,13 @@ int main() {
     cin >> a.at(i);
   }
 
+  // 単純にa.at(i+1)/a.at(i) == a.at(i+2)/a.at(i+1)で比較すると
+  // 公比が負の数の場合に整数に丸めたときに誤差が出てしまう
+  // 分母分子を襷掛けして、a.at(i+1)^2 == a.at(i)*a.at(i+2)で比較する
+  // aの数のが大きい場合があるので分数にする時にint64_t(long long)にする
   for (int i = 0; i < N - 2; i++) {
     if ((int64_t)a.at(i + 1) * a.at(i + 1) != (int64_t)a.at(i) * a.at(i + 2)) {
-      cout << "No" << endl;
+      cout << "No" << endl;  // 一つでも条件に合わなければプログラムを終了
       return 0;
     }
   }
