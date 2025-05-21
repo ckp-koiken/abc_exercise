c6f03608eaaeb95efd1cb0a0a74bc11b62b3ac29
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-21 10:19:06 +0900

    add another answer

--- 389/B_tcaF.cpp
+++ 389/B_tcaF.cpp
@@ -14,3 +14,29 @@ int main() {
   cout << N - 1 << endl;
   return 0;
 }
+
+// another answer
+// 階乗を計算する関数
+// int64_t fact(int n) {
+//   int64_t res = 1;
+//   for (int i = 1; i <= n; i++) {
+//     res *= i;
+//   }
+//   return res;
+// }
+//
+// int main() {
+//   int64_t x;
+//   cin >> x;
+//   // 単純にnを1ずつ増やして掛け算を繰り返し、
+//   // xと等しくなったらプログラムを止める
+//   int n = 1;
+//   for(;;) {
+//     if (fact(n) == x) {
+//       cout << n << endl;
+//       return 0;
+//     }
+//     n++;
+//   }
+//   return 0;
+// }
