134b1cdbf87f3ba5f1276810b90817049ed96b75
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-21 09:55:57 +0900

    add memo & simple fix

--- 389/A_9x9.cpp
+++ 389/A_9x9.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main() {
   string S;
   cin >> S;
-  int x = (int)S.at(0) - 48;
-  int y = (int)S.at(2) - 48;
+  int x = S.at(0) - 48;
+  // int x = S.at(0) - '0';
+  int y = S.at(2) - 48;
+  // int y = S.at(2) - '0'; // '0'を引くのでもいい
   cout << x * y << endl;
   return 0;
 }
