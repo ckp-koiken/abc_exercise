b7578ddc5022ed9252f8bc429e4077c068452ed9
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-25 19:52:23 +0900

    add another answer

--- 386/B_Calculator.cpp
+++ 386/B_Calculator.cpp
@@ -22,5 +22,20 @@ int main() {
   }
 
   cout << res << endl;
+
+  // another answer
+  // int ans = S.size();
+  // for (int i = 0; i < S.size(); i++) {
+  //   // substr()...i番目からn要素の部分文字列を取得する
+  //   if (S.substr(i, 2) == "00") {
+  //     // 00と連続した部分が見つかればその分入力回数を減らす
+  //     ans--;
+  //     // 00とすでに入力した場所は適当な文字に置き換える
+  //     S.at(i) = 'X';
+  //     S.at(i + 1) = 'X';
+  //   }
+  // }
+
+  // cout << ans << endl;
   return 0;
 }
