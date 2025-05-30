4caecb5292a61636144d792b7646eeff91c36f04
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-30 13:46:57 +0900

    solve B prob

---
+++ 384/B_ARC_Division.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  int N, R;
+  cin >> N >> R;
+  vector<int> D(N);
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> D.at(i) >> A.at(i);
+  }
+
+  for (int i = 0; i < N; i++) {
+    if (D.at(i) == 1) {
+      if (R >= 1600 && R <= 2799) {
+        R += A.at(i);
+      }
+    }
+
+    if (D.at(i) == 2) {
+      if (R >= 1200 && R <= 2399) {
+        R += A.at(i);
+      }
+    }
+  }
+
+  cout << R << endl;
+  return 0;
+}
+
+
+/*
+int main() {
+  int N, R;
+  cin >> N >> R;
+  
+  for (int i = 0; i < N; i++) {
+    int D, A;
+    cin >> D >> A;
+
+    if (D == 1) {
+      if (R >= 1600 && R <= 2799) {
+        R += A;
+      }
+    }
+
+    if (D == 2) {
+      if (R >= 1200 && R <= 2399) {
+        R += A;
+      }
+    }
+  }
+
+  cout << R << endl;
+  return 0;
+}
+*/ 
