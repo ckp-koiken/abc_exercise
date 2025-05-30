34f48b5c56a6105fac9b4df909691a4e95e784d0
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-30 13:49:12 +0900

    add another answer

--- 384/B_ARC_Division.cpp
+++ 384/B_ARC_Division.cpp
@@ -29,30 +29,29 @@ int main() {
   return 0;
 }
 
-
-/*
-int main() {
-  int N, R;
-  cin >> N >> R;
-  
-  for (int i = 0; i < N; i++) {
-    int D, A;
-    cin >> D >> A;
-
-    if (D == 1) {
-      if (R >= 1600 && R <= 2799) {
-        R += A;
-      }
-    }
-
-    if (D == 2) {
-      if (R >= 1200 && R <= 2399) {
-        R += A;
-      }
-    }
-  }
-
-  cout << R << endl;
-  return 0;
-}
-*/ 
+// another answer
+// 配列にしないで読み取りながらレートを判断する
+// int main() {
+//   int N, R;
+//   cin >> N >> R;
+//   
+//   for (int i = 0; i < N; i++) {
+//     int D, A;
+//     cin >> D >> A;
+// 
+//     if (D == 1) {
+//       if (R >= 1600 && R <= 2799) {
+//         R += A;
+//       }
+//     }
+// 
+//     if (D == 2) {
+//       if (R >= 1200 && R <= 2399) {
+//         R += A;
+//       }
+//     }
+//   }
+// 
+//   cout << R << endl;
+//   return 0;
+// }
