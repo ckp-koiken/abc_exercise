f96e34dd2b5e3ca1044d26700a5d3a5f3e933bae
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-03 15:11:35 +0900

    add memo

--- 407/A_Approximation.cpp
+++ 407/A_Approximation.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main() {
   double A, B;
+  // int A, B;  // 整数で受け取ってからキャストするのでも良い
   cin >> A >> B;
 
   double ans = round(A / B);
   cout << ans << endl;
+  // cout << round(static_cast<double>(A) / B) << endl;
   return 0;
 }
