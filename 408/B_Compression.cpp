f274521203f0478efa6fab227601faaf2a1a4fd0
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-05 23:50:34 +0900

    add memmo

--- 408/B_Compression.cpp
+++ 408/B_Compression.cpp
@@ -8,9 +8,11 @@ int main() {
   for (int i = 0; i < N; i++) {
     cin >> A.at(i);
   }
-  sort(A.begin(), A.end());
 
+  sort(A.begin(), A.end());
   set<int> st;
+  // まとめて以下の書き方でもいい
+  // set<int> st(A.begin(), A.end());
   for (int x : A) {
     st.insert(x);
   }
