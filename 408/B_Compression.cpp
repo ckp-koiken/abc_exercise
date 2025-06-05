9053276d3da2051fc8e74ec7548c86b934ea24f5
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-05 23:41:15 +0900

    solve B prob

---
+++ 408/B_Compression.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+  sort(A.begin(), A.end());
+
+  set<int> st;
+  for (int x : A) {
+    st.insert(x);
+  }
+
+  cout << st.size() << endl;
+  for (int s : st) {
+    cout << s << " ";
+  }
+
+  cout << endl;
+
+  return 0;
+}
