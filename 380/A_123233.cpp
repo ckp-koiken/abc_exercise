af57541769f2d8e27597050d5c3ab53f2718ca1e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-07 01:23:22 +0900

    add 380 & solve A prob

---
+++ 380/A_123233.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  string n = to_string(N);
+  int one = 0;
+  int two = 0;
+  int three = 0;
+  for (char c : n) {
+    if (c == '1') one++;
+    if (c == '2') two++;
+    if (c == '3') three++;
+  }
+
+  if (one == 1 && two == 2 && three == 3) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  
+  return 0;
+}
