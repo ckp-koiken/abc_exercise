336c2c2d63bcc80e7aa50211d1411e8fda2e1a4a
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-09 02:15:04 +0900

    add answer & interpretaion

--- 409/B_Citation.cpp
+++ 409/B_Citation.cpp
@@ -9,15 +9,34 @@ int main() {
     cin >> A.at(i);
   }
 
-  for (int x = N; x >= 0; x--) {
-    int count = 0;
-    for (auto a : A) {
-      if (a >= x) count++;
+  // 0から順番にxをみていく（Nまで）
+  // 同時にAの要素と比較してx以上のものがあればカウント
+  // カウント数がx以上のものがあればそれを答えとする
+  int ans = 0;
+  for (int x = 0; x <= N; x++) {
+    int cnt = 0;
+    for (int i = 0; i < N; i++) {
+      if (x <= A.at(i)) {
+        cnt++;
+      }
     }
-    if (count >= x) {
-      cout << x << endl;
-      break;
+    if (x <= cnt) {
+      ans = x;
     }
   }
+  cout << ans << endl;
+
+  // MEMO: 公式解説
+  // 大きいものから順にやっているだけで上と同様
+  // for (int x = N; x >= 0; x--) {
+  //   int count = 0;
+  //   for (auto a : A) {
+  //     if (a >= x) count++;
+  //   }
+  //   if (count >= x) {
+  //     cout << x << endl;
+  //     break;
+  //   }
+  // }
   return 0;
 }
