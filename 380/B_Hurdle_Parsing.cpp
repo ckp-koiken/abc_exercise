4478b012a08ee63731267cd7a67ef084a45ab12f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-08 15:57:57 +0900

    add another answer

--- 380/B_Hurdle_Parsing.cpp
+++ 380/B_Hurdle_Parsing.cpp
@@ -6,6 +6,7 @@ int main() {
   cin >> S;
 
   int A = 0;
+  // vector<int> a; // 配列を用意して追加してく方法でもよい
   for (int i = 1; i < S.size(); i++) {
     if (S.at(i) == '-') {
       A++;
@@ -13,9 +14,14 @@ int main() {
     
     if (S.at(i) == '|') {
       cout << A << " ";
+      // a.push_back(A);
       A = 0;
     }
   }
+
+  // for (int x : a) {
+  //   cout << x << " ";
+  // }
 
   cout << endl;
 
