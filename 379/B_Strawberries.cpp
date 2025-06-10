f003aeadd60ba97f24fc2634215b2376102099ee
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-10 14:13:30 +0900

    another answer

--- 379/B_Strawberries.cpp
+++ 379/B_Strawberries.cpp
@@ -20,6 +20,22 @@ int main() {
     }
   }
 
+  // another answer
+  // 左から順に歯を使って食べていって
+  // 使った歯を順にXに変更していくのは同じ
+  // int ans = 0;
+  // string t = string(K, 'O');  // K文字分連続したOの文字列を作る
+  // // N-K+1文字目まで見て、tと一致する箇所を探す
+  // for (int i = 0; i < N - K + 1; i++) {
+  //   // substr()を使うとi番目からK文字分の文字列集合を抽出するのに便利
+  //   if (S.substr(i, K) == t) {
+  //     ans++;
+  //     for (int j = i; j < i + K; j++) {
+  //       S.at(j) = 'X';
+  //     }
+  //   }
+  // }
+
   cout << ans << endl;
   return 0;
 }
