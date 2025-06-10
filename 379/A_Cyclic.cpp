7883f44edb6e4ad34f0a6ad4e7f52aa1124c0a1c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-10 13:41:58 +0900

    add memo

--- 379/A_Cyclic.cpp
+++ 379/A_Cyclic.cpp
@@ -6,6 +6,9 @@ int main() {
   cin >> N;
   string n = to_string(N);
   char a = n.at(0), b = n.at(1), c = n.at(2);
+  // 1文字ずつ受け取るのでもいい
+  // char a, b, c;
+  // cin >> a >> b >> c
 
   cout << b << c << a << " " << c << a << b << endl;
   return 0;
