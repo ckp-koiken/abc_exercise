b3f15658f3e4e4a9c562bcfccab1daa7fd4d0c18
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-13 14:40:33 +0900

    add another answer

--- 377/A_Rearranging_ABC.cpp
+++ 377/A_Rearranging_ABC.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
   string S;
   cin >> S;
+
   sort(S.begin(), S.end());
   if (S == "ABC") {
     cout << "Yes" << endl;
@@ -12,3 +13,11 @@ int main() {
   }
   return 0;
 }
+
+  // another answer
+  // findを使う
+  // if (S.find("A") != string::npos && S.find("B") != string::npos && S.find("C") != string::npos) {
+  //   cout << "Yes" << endl;
+  // } else {
+  //   cout << "No" << endl;
+  // }
