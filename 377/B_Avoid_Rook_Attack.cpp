03845a6740010ae94ab68c1de811ecd2adf73df6
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-14 14:55:38 +0900

    add interpretation

--- 377/B_Avoid_Rook_Attack.cpp
+++ 377/B_Avoid_Rook_Attack.cpp
@@ -7,17 +7,22 @@ int main() {
     cin >> S.at(i);
   }
 
+  // それぞれのマスを見ていって、置ける場所の数を数える
   int ans = 0;
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
+      // 駒があった場所は置けないのでスルー
       if (S.at(i).at(j) == '#') continue;
       bool ok = true;
+      // i行目を見て駒があれば置けないのでfalse
       for (int k = 0; k < 8; k++) {
         if (S.at(i).at(k) == '#') ok = false;
       }
+      // j列目を見て駒があれば置けないのでfalse
       for (int k = 0; k < 8; k++) {
         if (S.at(k).at(j) == '#') ok = false;
       }
+      // trueの数を数える
       if (ok) ans++;
     }
   }
