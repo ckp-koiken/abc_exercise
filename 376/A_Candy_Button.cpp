7592402a93f6ae4b23769352686f1334dd827321
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-15 21:36:49 +0900

    add interpretation

--- 376/A_Candy_Button.cpp
+++ 376/A_Candy_Button.cpp
@@ -5,11 +5,15 @@ int main() {
   int N, C;
   cin >> N >> C;
 
+  // キャンディーの数（初期1個）と前回もらった時間を保存
+  // 最初は必ずキャンディーをもらえるので一番最初のタイミングをpreに読み取る
   int ans = 1, pre;
   cin >> pre;
   for (int i = 1; i < N; i++) {
+    // その次の時間からTを受け取る
     int T;
     cin >> T;
+    // Tとpreの差がC以上ならキャンディーをもらえる
     if (T - pre >= C) {
       ans++;
       pre = T;
