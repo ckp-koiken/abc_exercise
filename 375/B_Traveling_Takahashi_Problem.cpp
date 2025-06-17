9ff4590a8988ecd3ea8c74f9f9ef4e1e5db73af3
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-17 15:13:15 +0900

    solve B prob

---
+++ 375/B_Traveling_Takahashi_Problem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  double X, Y;
+  cin >> N;
+  X = 0, Y = 0;
+
+  double cost = 0;
+  double x, y;
+  for (int i = 0; i < N; i++) {
+    cin >> x >> y;
+    cost += sqrt((x - X) * (x - X) + (y - Y) * (y - Y));
+    X = x;
+    Y = y;
+  }
+
+  cost += sqrt(x * x + y * y);
+
+  printf("%.10f\n", cost);
+  return 0;
+}
