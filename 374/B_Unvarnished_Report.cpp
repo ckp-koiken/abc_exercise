a87b82fbba40f9c839cec368106ad240f9208b74
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-20 08:43:09 +0900

    add another answer

--- 374/B_Unvarnished_Report.cpp
+++ 374/B_Unvarnished_Report.cpp
@@ -15,7 +15,7 @@ int main() {
 
   int cnt = 0;
   for (int i = 0; i < n; i++) {
-    if (S[i] == T[i]) {
+    if (S[i] == T[i]) { // 範囲外参照になる可能性がある
       cnt++;
     } else {
       cnt++;
@@ -23,6 +23,19 @@ int main() {
       return 0;
     }
   }
+
+  // another answer
+  // 小さい方をnにする
+  // n = min(S.size(), T.size());
+
+  // cntを作らず、S[i] != T[i]と判定された時点でi+1を返せば
+  // 範囲外参照を生じない
+  // for (int i = 0; i < n; i++) {
+  //   if (S[i] != T[i]) {
+  //     cout << (i + 1) << endl;
+  //     return 0;
+  //   }
+  // }
 
   cout << 0 << endl;
   return 0;
