eecf76eb5de0370550cdd7101c2db59d84ffad54
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-21 22:21:50 +0900

    add 373 & solve A prob

---
+++ 373/A_September.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int ans = 0;
+  for (int i = 1; i <= 12; i++) {
+    string S;
+    cin >> S;
+    if (S.size() == i) ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
