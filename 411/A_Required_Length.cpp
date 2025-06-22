362e59d6fd3e2bc4283b9b558cfec71621c0bcd2
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-22 22:52:12 +0900

    add 411 & solve A prob

---
+++ 411/A_Required_Length.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string P;
+  int L;
+  cin >> P >> L;
+
+  if (P.size() >= L) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  
+  return 0;
+}
