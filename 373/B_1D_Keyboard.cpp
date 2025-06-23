acb7d0a573dd7bc243d33642bf9488e415de5aa3
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-23 15:52:36 +0900

    add interpretation

--- 373/B_1D_Keyboard.cpp
+++ 373/B_1D_Keyboard.cpp
@@ -5,12 +5,15 @@ int main() {
   string S;
   cin >> S;
 
+  // キーそれぞれのAからの距離を記録
   vector<int> key(26);
   for (int i = 0; i < 26; i++) {
+    // Aからの距離に応じてキーに0から25を振る
     key.at(S.at(i) - 'A') = i;
   }
 
   int ans = 0;
+  // 現在のアルファベットと次のアルファベットとでキーの距離の差を取って足していく
   for (int i = 0; i < 25; i++) {
     ans += abs(key.at(i) - key.at(i + 1));
   }
