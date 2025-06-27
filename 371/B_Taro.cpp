efb61c926b6768168f6c3acb04d7d0fbcb931edc
ckp-koiken <kentaro.koi0710@gmail.com>
2025-06-27 16:20:04 +0900

    add another answer

--- 371/B_Taro.cpp
+++ 371/B_Taro.cpp
@@ -21,3 +21,31 @@ int main() {
   }
   return 0;
 }
+
+// another answer
+// int main() {
+//   int n, m;
+//   cin >> n >> m;
+  
+//   // 各家庭で長男が生まれたかどうかを管理する配列を用意
+//   vector<bool> taro(n + 1);
+//   for (int i = 0; i < m; i++) {
+//     int a; 
+//     char b;
+//     cin >> a >> b;
+//     if (b == 'M') {
+//       // bがMで、配列taroにフラグが立っていればもう生まれているのでNo
+//       if (taro.at(a)) {
+//         cout << "No" << endl;
+//       } else {
+//         // フラグが立っていなければフラグを立てたうえでYes
+//         taro.at(a) = true;
+//         cout << "Yes" << endl;
+//       }
+//     } else {
+//       // 女の子なのでNo
+//       cout << "No" << endl;
+//     }
+//   }
+//   return 0;
+// }
