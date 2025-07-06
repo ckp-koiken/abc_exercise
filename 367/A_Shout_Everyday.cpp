85e56e07b7d00520ca74df49ba018546c567e429
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-06 22:45:55 +0900

    add interpretation

--- 367/A_Shout_Everyday.cpp
+++ 367/A_Shout_Everyday.cpp
@@ -4,16 +4,21 @@ using namespace std;
 int main() {
   int A, B, C;
   cin >> A >> B >> C;
+  // 日をまたいで就寝しているときと日をまたがない時とで場合わけする
+  // 例）21-7就寝、8-14就寝
 
+  // 日をまたがない場合(B < C)
+  // 寝てる時間で判断
   if (B < C) {
-    if (B < A && A < C) {
+    if (B < A && A < C) { // 寝ている時間は叫べない
       cout << "No" << endl;
     } else {
       cout << "Yes" << endl;
     }
-  } else {
+  } else {  // 日をまたぐ場合(C < B)
+    // 起きている時間で判断
     if (C < A && A < B) {
-      cout << "Yes" << endl;
+      cout << "Yes" << endl;  // 起きている時間であれば叫べる
     } else {
       cout << "No" << endl;
     }
