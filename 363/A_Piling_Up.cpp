753400f083bbec660ba32bec9fc0d02419ba437f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-13 19:43:02 +0900

    add 363 & solve A prob

---
+++ 363/A_Piling_Up.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int R;
+  cin >> R;
+
+  if (R >= 1 && R <= 99) {
+    cout << 100 - R << endl;
+  }
+
+  if (R >= 100 && R <= 199) {
+    cout << 200 - R << endl;
+  }
+
+  if (R >= 200 && R <= 299) {
+    cout << 300 - R << endl;
+  }
+
+  return 0;
+}
