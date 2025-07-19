4a797b44d97f6ab0d35beee6fab322ea183da2f2
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-19 20:32:07 +0900

    add 361 & solve A prob

---
+++ 361/A_Insert.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, K, X;
+  cin >> N >> K >> X;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+
+  for (int i = 0; i < K; i++) {
+    cout << A.at(i) << " ";
+  }
+  cout << X << " ";
+
+  for (int i = K; i < N; i++) {
+    cout << A.at(i) << " ";
+  }
+
+  cout << endl;
+  return 0;
+}
