9f3446335679aeb1cc98cda5e6bc777d4ff2f414
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-19 20:40:31 +0900

    add another answer

--- 361/A_Insert.cpp
+++ 361/A_Insert.cpp
@@ -21,3 +21,24 @@ int main() {
   cout << endl;
   return 0;
 }
+
+// another answer
+// 実際に配列のK番目の次にXを挿入する
+// insert()を使う
+// int main (){
+//   int N, K, X;
+//   cin >> N >> K >> X;
+//   vector<int> A(N);
+//   for (int i = 0; i < N; i++) {
+//     cin >> A.at(i);
+//   }
+
+//   A.insert(A.begin() + K, X);
+
+//   for (int a : A) {
+//     cout << a << " ";
+//   }
+//   cout << endl;
+
+//   return 0;
+// }
