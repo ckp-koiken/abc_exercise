6504084041afce5e46ffe5b9881c9b8c94c21739
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-22 18:08:28 +0900

    add 359

---
+++ 359/A_Count_Takahashi.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  int ans = 0;
+  string S;
+  for (int i = 0; i < N; i++) {
+    cin >> S;
+    if (S == "Takahashi") ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
