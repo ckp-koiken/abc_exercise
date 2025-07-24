6b88c1d0459f24e5212fb8e10e5ed967a32b194c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-24 20:47:41 +0900

    add another answer

--- 415/B_Pick_Two.cpp
+++ 415/B_Pick_Two.cpp
@@ -24,5 +24,13 @@ int main() {
       cnt = 0;
     }
   }
+
+  // another answer
+  // 1文字ずつカンマを入れて2文字ごとに改行して表示する方法
+  // iとi+1番目の数字をカンマ入りで表示して改行する
+  // インデックスを2文字ずつ進める
+  for (int i = 0; i < pos.size(); i += 2) {
+    cout << pos.at(i) << "," << pos.at(i + 1) << endl;
+  }
   return 0;
 }
