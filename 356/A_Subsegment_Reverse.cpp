93eebef3dfb690488f6332d43013561809ffeb34
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-27 22:29:24 +0900

    add another answer

--- 356/A_Subsegment_Reverse.cpp
+++ 356/A_Subsegment_Reverse.cpp
@@ -19,3 +19,26 @@ int main() {
 
   return 0;
 }
+
+// another answer
+// 逆の部分も含め、直接配列を作ってしまう
+// int main() {
+//   int n, l, r;
+//   cin >> n >> l >> r;
+//   vector<int> a(n);
+
+//   for (int i = 0; i < n; i++) {
+//     a.at(i) = i + 1;
+//   }
+//   // reverse()に反転したい区間を渡す
+//   // 反転を開始したい位置の添え字(l - 1)と反転を終了した次の添え字(r)を渡す（イテレータ）
+//   // 半開区間
+//   reverse(a.begin() + (l - 1), a.begin() + r);
+
+//   for (int x : a) {
+//     cout << x << " ";
+//   }
+
+//   cout << endl;
+//   return 0;
+// }
