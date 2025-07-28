d8e0af0be2fc85a0b1f19cee2ba4e4f4d0a7dc8f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-29 00:53:25 +0900

    add memo

--- 354/B_AtCoder_Janken2.cpp
+++ 354/B_AtCoder_Janken2.cpp
@@ -10,12 +10,15 @@ int main() {
     cin >> S.at(i) >> C.at(i);
   }
 
+  // レートの合計を算出
   int total = 0;
   for (int c : C) {
     total += c;
   }
+  // modを計算
   int mod = total % N;
 
+  // Sを辞書順に並び替える
   for (int i = 0; i < N - 1; i++) {
     for (int j = 0; j < N - 1; j++) {
       if (S.at(j) > S.at(j + 1)) {
