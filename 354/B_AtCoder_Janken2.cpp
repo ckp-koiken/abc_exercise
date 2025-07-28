18819eb2fb122f3cf05f540851d9a773e168eb3c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-07-29 00:51:16 +0900

    solve B prob

---
+++ 354/B_AtCoder_Janken2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<string> S(N);
+  vector<int> C(N);
+  for (int i = 0; i < N; i++) {
+    cin >> S.at(i) >> C.at(i);
+  }
+
+  int total = 0;
+  for (int c : C) {
+    total += c;
+  }
+  int mod = total % N;
+
+  for (int i = 0; i < N - 1; i++) {
+    for (int j = 0; j < N - 1; j++) {
+      if (S.at(j) > S.at(j + 1)) {
+        swap(S.at(j), S.at(j + 1));
+      }
+    }
+  }
+
+  cout << S.at(mod) << endl;
+
+  return 0;
+}
