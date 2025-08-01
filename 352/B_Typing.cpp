a271b1c85c7a1471d6beba6f217b9829f925634c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-01 23:28:17 +0900

    remove memo: sがabccなどを考えていなかった

--- 352/B_Typing.cpp
+++ 352/B_Typing.cpp
@@ -18,23 +18,5 @@ int main() {
 
   cout << endl;
 
-  // MEMO: 当初はこれで書いていた。何が間違っているのかよくわからない。
-  // sampleとテストケース7つはACするが、後半のテストケースはWAになる
-  // Sの含まれる文字だが、2回連続入力してしまった、なども誤って入力した文字に含まれるのか？
-  // SがabcでTがaxbxcycなど。cを間違って2回入力している？（どっちのcが正しいか判断できない）
-
-  // Sの各文字をsetにまとめてTと一致した文字の番号を表示する
-  // set<char> st;
-  // for (int i = 0; i < (int)S.size(); i++) {
-  //   st.insert(S.at(i));
-  // }
-
-  // for (int i = 0; i < (int)T.size(); i++) {
-  //   if (st.count(T.at(i))) {
-  //     cout << i + 1 << " ";
-  //   }
-  // }
-  // cout << endl;
-
   return 0;
 }
