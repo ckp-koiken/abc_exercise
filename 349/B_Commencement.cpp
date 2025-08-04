459c3ce966a7f3bc99053c44e13abf641e2df0c1
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-04 22:58:26 +0900

    add another answer

--- 349/B_Commencement.cpp
+++ 349/B_Commencement.cpp
@@ -13,6 +13,8 @@ int main() {
       data[S.at(i)] = 1;
     }
   }
+  // 以下でも書ける
+  // for (char c : S) data[c]++;
   
   for (int index = 1; index <= 100; index++) {
     int cnt = 0;
@@ -24,6 +26,18 @@ int main() {
       return 0;
     }
   }
+
+  // another answer
+  // 文字の出現数ごとの数を数えるためのmapをもう一つ作る
+  // 出現数ごとの種類数が2でなければNo（出現数0はそもそもカウントされない）
+  // map<int, int> cnt;
+  // for (auto p : data) cnt[p.second]++;
+  // for (auto p : cnt) {
+  //   if (p.second != 2) {
+  //     cout << "No" << endl;
+  //     return 0;
+  //   }
+  // }
 
   cout << "Yes" << endl;
   return 0;
