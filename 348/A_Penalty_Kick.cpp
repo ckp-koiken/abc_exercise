af98b0a7e1358eb05c4e9356050fc0a36dc1520a
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-05 16:49:40 +0900

    add another answer

--- 348/A_Penalty_Kick.cpp
+++ 348/A_Penalty_Kick.cpp
@@ -14,3 +14,15 @@ int main() {
   cout << endl;
   return 0;
 }
+
+// another answer
+// 空の文字列にoかxを足していって文字列全体を表示する方法
+// string S = "";
+// for (int i = 1; i <= N; i++) {
+//   if (i % 3 == 0) {
+//     S.push_back('x');
+//   } else {
+//     S.push_back('o');
+//   }
+// }
+// cout << S << endl;
