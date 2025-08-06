93f6928cd2fe17d140aedfb844e3b726a640a6f6
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-06 21:00:11 +0900

    add interpretation

--- 347/B_Substring.cpp
+++ 347/B_Substring.cpp
@@ -5,13 +5,20 @@ int main() {
   string S;
   cin >> S;
 
+  // 部分文字列を列挙し重複を除いて数える
+  // substr()で抽出してsetを使って数える
   set<string> st;
+  
   for (int i = 0; i < S.size(); i++) {
+    // 部分文字列の開始位置iを指定するためのループ
     for (int j = 1; i + j <= S.size(); j++) {
+      // 部分文字列の抽出部分jを指定するためのループ（開始が1）
+      // 開始位置iと抽出部分jの合計がSの大きさになるまでループする
       st.insert(S.substr(i, j));
     }
   }
 
+  // setの要素数が答えになる
   cout << st.size() << endl;
   return 0;
 }
