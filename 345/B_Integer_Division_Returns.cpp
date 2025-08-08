04f8da61e5b7e1ee65f1f417c843b5fd0a58894e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-08 16:32:45 +0900

    add interpretation

--- 345/B_Integer_Division_Returns.cpp
+++ 345/B_Integer_Division_Returns.cpp
@@ -4,15 +4,20 @@ using namespace std;
 int main() {
   int64_t X;
   cin >> X;
+  // 正の数を割り算（/）をすると切り捨てになる
+  // 逆に負の数を割り算すると切り上げになる
 
+  // 10で割ったときの最小の整数を求めたい
   int64_t ans;
   if (X >= 0) {
+    // Xが正の時、切り上げにしたいのでXに9を足す
     ans = (X + 9) / 10;
   } else {
+    // Xが負の時はそのままでよい
     ans = X / 10;
   }
 
   cout << ans << endl;
-  
+
   return 0;
 }
