a920159df4515ec80c732690ddcab92f1a4bcff6
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-12 21:28:36 +0900

    add another answer

--- 417/A_A_Substring.cpp
+++ 417/A_A_Substring.cpp
@@ -8,5 +8,14 @@ int main() {
   
   cout << S.substr(A, N - A - B) << endl;
 
+  // another answer
+  // 消される文字の順番をN, A, Bで指定する
+  // int start = A;
+  // int end = N - B;
+  // for (int i = start; i < end; i++) {
+  //   cout << S.at(i);
+  // }
+  // cout << endl;
+
   return 0;
 }
