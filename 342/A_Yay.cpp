4f30932e781bc69bb303c4fe72f68c5d1d23cf09
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-14 14:29:08 +0900

    add another answer

--- 342/A_Yay.cpp
+++ 342/A_Yay.cpp
@@ -25,3 +25,22 @@ int main() {
   }
   return 0;
 }
+
+// another answer
+// 二重ループを使って異なる文字にフラグを立てる
+
+// フラグを設定するループ
+// for (int i = 0; i < (int)S.size(); i++) {
+//   bool diff = true; // フラグ
+//   // フラグの対象を文字列全体にわたって探索するループ
+//   for (int j = 0; j < (int)S.size(); j++) {
+//     // iとjが異なるとき、フラグ対象と文字が等しければフラグを下げる
+//     if (i != j && S.at(i) == S.at(j)) {
+//       diff = false;
+//     }
+//   }
+//   // フラグが立っていれば異なる文字なので番号を表示する
+//   if (diff == true) {
+//     cout << i + 1 << endl;
+//   }
+// }
