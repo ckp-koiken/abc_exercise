30936a550514efeea6dec574f7969b11f7162d4a
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-20 19:33:38 +0900

    add interpretation

--- 337/B_ExtendedABC.cpp
+++ 337/B_ExtendedABC.cpp
@@ -5,6 +5,8 @@ int main() {
   string S;
   cin >> S;
 
+  // A、B、Cそれぞれ100文字ずつ連結するすべての組み合わせを試して
+  // Sと一致するものがあればYes
   for (int a = 0; a < 100; a++) {
     for (int b = 0; b < 100; b++) {
       for (int c = 0; c < 100; c++) {
