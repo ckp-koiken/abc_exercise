b918ca5756b9b7e7a9b3cc430f0fc70af7ab554e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-20 19:45:17 +0900

    add another answer

--- 337/B_ExtendedABC.cpp
+++ 337/B_ExtendedABC.cpp
@@ -21,3 +21,23 @@ int main() {
   cout << "No" << endl;
   return 0;
 }
+
+// another answer
+// 入力された文字列Sがソート済みであるかどうかを確かめればよい
+// int main() {
+//   string s;
+//   cin >> s;
+
+//   string t = s;
+//   sort(t.begin(), t.end());
+//   if (s == t) {
+//     cout << "Yes" << endl;
+//   } else {
+//     cout << "No" << endl;
+//   }
+
+//   // 実際にソートしなくても隣同士の並びを確認するのでも良い
+//   // Bの直後にあるのはBかC
+//   // Cの直後にあるのはCのみ
+//   return 0;
+// }
