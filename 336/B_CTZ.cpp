076e1853de95e8eeae573cff2c36fbea4419077e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-21 09:51:17 +0900

    add another answer

--- 336/B_CTZ.cpp
+++ 336/B_CTZ.cpp
@@ -19,3 +19,19 @@ int main() {
   cout << ans << endl;
   return 0;
 }
+
+// another answer
+// nをi(>0)だけ順番に右シフトする
+// 1と論理積をとって1になった時のiが答え
+// nを固定してiを左シフトするのでも良い
+// 2進数nの各桁と1の論理積を順番にとって判断する方法
+// int main() {
+//   int n;
+//   cin >> n;
+//   for (int i = 0;; i++) {
+//     if ((n >> i) & 1) {
+//       cout << i << endl;
+//       return 0;
+//     }
+//   }
+// }
