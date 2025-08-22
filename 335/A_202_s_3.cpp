7dbd3c857793831e632481f87ffa4d8e16f410e0
ckp-koiken <kentaro.koi0710@gmail.com>
2025-08-22 09:53:51 +0900

    add another answer

--- 335/A_202_s_3.cpp
+++ 335/A_202_s_3.cpp
@@ -8,6 +8,12 @@ int main() {
 
   S.at(n - 1) = '4';
 
+  // another answer
+  // pop_backで末尾を削除して
+  // push_backで末尾に4を追加するのでも良い
+  // S.pop_back();
+  // S.push_back('4');
+
   cout << S << endl;
   return 0;
 }
