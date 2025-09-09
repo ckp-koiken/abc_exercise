d42a394e643c0836e9babc053bf575aad7367c75
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-09 16:03:58 +0900

    add another answer

--- 326/B_326like_Numbers.cpp
+++ 326/B_326like_Numbers.cpp
@@ -17,3 +17,28 @@ int main() {
 
   return 0;
 }
+
+// another answer
+// to_stringを使わないで各桁の数を直接計算してもOK
+
+// bool check(int n) {
+//   // 100の位の数字は100で割った商
+//   int c100 = n / 100;
+//   // 10の位の数字は10で割った商を10で割った余り
+//   int c10 = n / 10 % 10;
+//   // 1の位の数字は10で割った余り
+//   int c1 = n % 10;
+//   return c100 * c10 == c1;
+// }
+
+// int main() {
+//   int n;
+//   cin >> n;
+//   for (int i = n; i <= 919; i++) {
+//     if (check(i)) {
+//       cout << i << endl;
+//       return 0;
+//     }
+//   }
+//   return 0;
+// }
