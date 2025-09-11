24119f6737d0c8a8d9ef2e860e0401c67741d79f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-11 14:14:42 +0900

    add another answer

--- 324/A_Same.cpp
+++ 324/A_Same.cpp
@@ -18,4 +18,21 @@ int main() {
 
   cout << "Yes" << endl;
   return 0;
+
+  // another answer
+  // 先頭だけ先に読み込んでおいて残りがすべて先頭と等しいかを判定すればよい
+  // 配列として読み込むより高速
+  // int n, a1;
+  // cin >> n >> a1;
+  // for (int i = 1; i < n; i++) {
+  //   int a;
+  //   cin >> a;
+  //   if (a != a1) {
+  //     cout << "No" << endl;
+  //     return 0;
+  //   }
+  // }
+
+  // cout << "Yes" << endl;
+  // return 0;
 }
