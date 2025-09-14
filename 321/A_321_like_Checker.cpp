3573c264e600ce8f70aa72632f30a3227aa5d5d4
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-14 13:04:03 +0900

    add another answer

--- 321/A_321_like_Checker.cpp
+++ 321/A_321_like_Checker.cpp
@@ -11,7 +11,7 @@ int main() {
   // MEMO: そのまま文字列として受け取ってもいい
   // string s;
   // cin >> s;
-  
+
   for (int i = 0; i < d - 1; i++) {
     if (S[i] <= S[i + 1]) {
       cout << "No" << endl;
@@ -23,3 +23,29 @@ int main() {
 
   return 0;
 }
+
+// another answer
+// Nを10で割り続けて各桁に分解する
+// 桁の数値を配列に格納して比較する（反転してから比較）
+// int main() {
+//   int n;
+//   cin >> n;
+//   vector<int> d;
+
+//   while (n > 0) {
+//     d.push_back(n % 10);
+//     n /= 10;
+//   }
+//   // 桁が逆順に格納されるので逆にしてから比較する
+//   reverse(d.begin(), d.end());
+
+//   for (int i = 0; i < (int)d.size() - 1; i++) {
+//     if (d.at(i) <= d.at(i + 1)) {
+//       cout << "No" << endl;
+//       return 0;
+//     }
+//   }
+
+//   cout << "Yes" << endl;
+//   return 0;
+// }
