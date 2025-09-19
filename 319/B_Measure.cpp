cd51baeca5310d8c8bb82709da32b3df97eb1760
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-19 14:49:19 +0900

    add memo

--- 319/B_Measure.cpp
+++ 319/B_Measure.cpp
@@ -7,13 +7,19 @@ int main() {
 
   string S;
   for (int i = 0; i <= N; i++) {
+    // 先に-で埋めておく
     S.push_back('-');
+
+    // 1から9まで調べる
     for (int j = 1; j <= 9; j++) {
       if (N % j == 0 && i % (N / j) == 0) {
+        // 条件に合致するjがあれば最初のものを(最小値なので)
+        // 文字として代入（+48）
         S.at(i) = j + 48;
         break;
       }
     }
+
   }
 
   cout << S << endl;
