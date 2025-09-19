77e8e710187ec11ea7e46f1318ee29074f560725
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-19 14:18:02 +0900

    add 319 & solve A prob

---
+++ 319/A_Legentdary_Players.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  map<string, int> ma;
+  ma["tourist"] = 3858;
+  ma["ksun48"] = 3679;
+  ma["Benq"] = 3658;
+  ma["Um_nik"] = 3648;
+  ma["apiad"] = 3638;
+  ma["Stonefeang"] = 3630;
+  ma["ecnerwala"] = 3613;
+  ma["mnbvmar"] = 3555;
+  ma["newbiedmy"] = 3516;
+  ma["semiexp"] = 3481;
+
+  string S;
+  cin >> S;
+
+  cout << ma[S] << endl;
+  return 0;
+}
