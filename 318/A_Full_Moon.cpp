f3d853d29099ef6f9fabede42ae8bc74a3e09165
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-20 14:50:38 +0900

    add another answer

--- 318/A_Full_Moon.cpp
+++ 318/A_Full_Moon.cpp
@@ -15,3 +15,21 @@ int main() {
   
   return 0;
 }
+
+// another answer
+// 実際にシミュレーションしてみる
+// m日目に最初の月を見て、そこからp日ごとに月をみる回数を数える
+// n日に達するまで続ける
+// int main() {
+//   int n, m, p;
+//   cin >> n >> m >> p;
+
+//   int res = 0;
+//   while (m <= n) {
+//     res++;
+//     m += p;
+//   }
+
+//   cout << res << endl;
+//   return 0;
+// }
