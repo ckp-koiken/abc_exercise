2befe7d8ad2551d723c7ac8e4535d8507173f5d2
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-25 22:16:44 +0900

    add another answer

--- 313/A_To_Be_Saikyo.cpp
+++ 313/A_To_Be_Saikyo.cpp
@@ -25,3 +25,22 @@ int main() {
 
   return 0;
 }
+
+// another answer
+// Pの2番目要素から最終要素までの中から最大値を最強とする
+// int main() {
+//   int N;
+//   cin >> N;
+//   vector<int> P(N);
+//   for (int i = 0; i < N; i++) {
+//     cin >> P.at(i);
+//   }
+
+//   // begin()+1として2番目要素から最後までの中で最大値を探す
+//   int max_p = *max_element(P.begin() + 1, P.end() + 1);
+
+//   // P.at(0)が最強の場合0を、
+//   // それ以外のどれかが最強の場合差をとって1を足したものを表示
+//   // maxを使う
+//   cout << max(0, max_p + 1 - P.at(0)) << endl;
+// }
