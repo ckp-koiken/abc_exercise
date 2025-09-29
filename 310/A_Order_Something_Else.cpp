8d02a759c6a59132a8eda713076c4381fe659755
ckp-koiken <kentaro.koi0710@gmail.com>
2025-09-29 11:13:10 +0900

    add another answer

--- 310/A_Order_Something_Else.cpp
+++ 310/A_Order_Something_Else.cpp
@@ -18,3 +18,22 @@ int main() {
 
   return 0;
 }
+
+// another answer
+// 最小値の求め方を工夫できる
+// int main() {
+//   int n, p, q;
+//   cin >> n >> p >> q;
+
+//   // 配列にせずにdでd_minの最小値を更新していく
+//   int d_min = 100000;
+//   int d;
+//   for (int i = 0; i < n; i++) {
+//     cin >> d;
+//     d_min = min(d_min, d);
+//   }
+
+//   // 割引を使わないpと割引を使ったq+d_minの小さい方を表示する
+//   cout << min (p, q + d_min) << endl;
+//   return 0;
+// }
