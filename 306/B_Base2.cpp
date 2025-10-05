1bc950e66666b81eeef5d10528d96b1008a84762
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-05 22:10:25 +0900

    add another answer

--- 306/B_Base2.cpp
+++ 306/B_Base2.cpp
@@ -20,3 +20,23 @@ int main() {
   cout << ans << endl;
   return 0;
 }
+
+// another answer
+// シフト演算子を使うとスマートに書ける
+
+// // 型の名前を簡略化
+// using ull = unsigned long long;
+// 
+// int main() {
+//   ull ans = 0;
+//   for (int i = 0; i < 64; i++) {
+//     ull a;
+//     cin >> a;
+//     
+//     // 2の累乗の数だけaを左シフトする
+//     // aが0の場合どんなに左シフトしても0のまま
+//     ans += a << i;
+//   }
+//   cout << ans << endl;
+//   return 0;
+// }
