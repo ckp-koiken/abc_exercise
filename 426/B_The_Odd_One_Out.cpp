542c969cb0a3a43e20adfc8261432c6ac556caa6
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-06 11:07:43 +0900

    add another answer

--- 426/B_The_Odd_One_Out.cpp
+++ 426/B_The_Odd_One_Out.cpp
@@ -22,3 +22,23 @@ int main() {
 
   return 0;
 }
+
+// another answer
+// Sを辞書順にソートして先頭か末尾を探す
+// ソートしたSの1文字目と2文字目が違うなら1文字目が答え
+// そうでないなら末尾の1文字が答え
+
+// int main() {
+//   string s;
+//   cin >> s;
+//   sort(s.begin(), s.end());
+//
+//   if (s[0] != s[1]) {
+//     cout << s[0] << endl;
+//   } else {
+//     // .back()で末尾要素を取得
+//     cout << s.back() << endl;
+//   }
+//
+//   return 0;
+// }
