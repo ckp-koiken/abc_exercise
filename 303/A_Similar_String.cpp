ff33da7661d0c2fb646ac4a2e386736385cfd073
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-09 20:08:04 +0900

    add 303 & solve A prob

---
+++ 303/A_Similar_String.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_similar(char x, char y) {
+  if (x == y) {
+    return true;
+  } else if ((x == '1' && y == 'l') || (x == 'l' && y == '1')) {
+    return true;
+  } else if ((x == '0' && y == 'o') || (x == 'o' && y == '0')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int main() {
+  int N;
+  string S, T;
+  cin >> N >> S >> T;
+
+  for (int i = 0; i < N; i++) {
+    if (!is_similar(S[i], T[i])) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+
+  cout << "Yes" << endl;
+
+  return 0;
+}
