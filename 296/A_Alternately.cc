0af089dd2b237c37445a9dd26efc5ff7c029e039
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-20 08:28:52 +0900

    add 296 & solve A prob

---
+++ 296/A_Alternately.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+
+  for (int i = 0; i < N - 1; i++) {
+    if (S[i] == S[i + 1]) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  
+  cout << "Yes" << endl;
+  return 0;
+}
