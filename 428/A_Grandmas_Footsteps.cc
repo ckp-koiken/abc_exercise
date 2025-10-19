347acfc786987382ce5d8d31d1d1beec6b411312
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-19 17:48:33 +0900

    add another answer

--- 428/A_Grandmas_Footsteps.cc
+++ 428/A_Grandmas_Footsteps.cc
@@ -20,3 +20,27 @@ int main() {
   cout << dis << endl;
   return 0;
 }
+
+// another answer
+// 算数的に解く
+// a+bの反復とa+bの余りに注目する
+// int main() {
+//   int s, a, b, x;
+//   cin >> s >> a >> b >> x;
+
+//   // 進んだ回数
+//   int len = 0;
+
+//   // xをa+bで割った商にaをかけたものが進んだ回数
+//   len += (x / (a + b)) * a;
+
+//   // 反復できない程度の時間しか残されていない場合
+//   // xをa+bで割った余りとaを比較して小さい方が進んだ回数
+//   len += min(x % (a + b), a);
+
+//   // 進んだ回数にsをかけたものが移動距離
+//   int ans = len * s;
+//   cout << ans << endl;
+
+//   return 0;
+// }
