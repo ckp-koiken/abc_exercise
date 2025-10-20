75f8c06b46f7bc256ed5b40e2fdd26c50c8fb74f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-20 11:44:11 +0900

    add interpretation

--- 296/C_Gap_Existence.cc
+++ 296/C_Gap_Existence.cc
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 愚直に全探索すると計算量が大きすぎてTLEする
+// setをうまく使う
 int main() {
   int N, X;
   cin >> N >> X;
@@ -9,15 +11,20 @@ int main() {
     cin >> A[i];
   }
 
+  // Aをsetに保持
   set<int> se(A.begin(), A.end());
 
+  // setの要素をそれぞれ確認
   for (auto a : se) {
+    // setの要素は昇順に並ぶので
+    // setの各要素にXを足したものが他の要素の中にあればよい
+    // つまり要素+Xの位置をfindで探してend()イテレータでなければyes
     if (se.find(a + X) != se.end()) {
       cout << "Yes" << endl;
       return 0;
     }
   }
   cout << "No" << endl;
-  
+
   return 0;
 }
