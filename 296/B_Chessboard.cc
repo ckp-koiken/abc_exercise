4bf58f67d1becbd5c8bb92c59a476a1b276fd8da
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-20 11:09:22 +0900

    add interpretation

--- 296/B_Chessboard.cc
+++ 296/B_Chessboard.cc
@@ -7,9 +7,13 @@ int main() {
     cin >> S[i];
   }
 
+  // *が書かれているマスを探す
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       if (S[i][j] == '*') {
+        // 左からの位置(添え字j)でabcdを決める
+        // 下からの位置で番号を決める
+        // 一番下が1なので上限の8からiを引いたもので求める
         cout << "abcdefgh"[j] << 8 - i << endl;
         return 0;
       }
