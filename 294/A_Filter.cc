209e7849035bd0417159bd985bc1f87ddf517eed
ckp-koiken <kentaro.koi0710@gmail.com>
2025-10-22 08:03:55 +0900

    add 294 & solve A prob

---
+++ 294/A_Filter.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    int A;
+    cin >> A;
+    if (A % 2 == 0) {
+      cout << A << " ";
+    }
+  }
+  cout << '\n';
+  return 0;
+}
