c29c4241a3b8497398ced3ab6b604d5133a5f277
ckp-koiken <kentaro.koi0710@gmail.com>
2025-11-01 13:30:21 +0900

    add 289 & solve A prob

---
+++ 289/A_flip.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  for (char c : s) {
+    if (c == '0') {
+      cout << '1';
+    } else {
+      cout << '0';
+    }
+  }
+
+  cout << '\n';
+  return 0;
+}
