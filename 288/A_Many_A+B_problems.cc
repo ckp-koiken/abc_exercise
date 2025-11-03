e5f04e4dfec631b8bc16d926f519ab8f7d026aaf
ckp-koiken <kentaro.koi0710@gmail.com>
2025-11-03 17:02:28 +0900

    add 288 & solve A prob

---
+++ 288/A_Many_A+B_problems.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  
+  for (int i = 0; i < N; i++) {
+    int64_t A, B;
+    cin >> A >> B;
+    cout << A + B << endl;
+  }
+
+  return 0;
+}
