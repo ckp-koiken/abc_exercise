4161d704aad44db1c098cdba9298eda73668eb7e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-11-09 19:12:34 +0900

    simple fix

---
+++ 431/B_Robot_Weight.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X, N, Q, P;
+  cin >> X >> N;
+  vector<int> W(N);
+  for (int i = 0; i < N; i++) cin >> W[i];
+  cin >> Q;
+
+  vector<bool> W_a(N);
+
+  for (int i = 0; i < Q; i++) {
+    int P;
+    cin >> P;
+
+    if (!W_a[P - 1]) {
+      W_a[P - 1] = true;
+      X += W[P - 1];
+    } else {
+      W_a[P - 1] = false;
+      X -= W[P - 1];
+    }
+
+    cout << X << endl;
+  }
+  return 0;
+}
