a935866e77a2cc8a05268f901ec7afc4f886455d
ckp-koiken <kentaro.koi0710@gmail.com>
2025-11-11 15:13:55 +0900

    add interpretation

--- 285/B_Longest_Uncommon_Prefix.cc
+++ 285/B_Longest_Uncommon_Prefix.cc
@@ -6,13 +6,22 @@ int main() {
   string S;
   cin >> N >> S;
 
+  // iが1からN-1まで全探索
   for (int i = 1; i < N; i++) {
+
+    // 問題文でのi+lを示すjが1からNまで全探索
     for (int j = 1; j <= N; j++) {
+
+      // j-1番目とiの数だけ離れたSの文字が同じとき
+      // そのときの位置であるj-1を出力
       if (S[j - 1] == S[i + j - 1]) {
-        cout << j - 1<< endl;
+        cout << j - 1 << endl;
         break;
       }
 
+      // i+j>Nのとき
+      // Sを全部探索しきったとき
+      // 最後の位置j-1を出力
       if (i + j > N) {
         cout << j - 1 << endl;
         break;
