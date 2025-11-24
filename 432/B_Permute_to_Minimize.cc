e66d14e13ed430bbf84aa67807d726d9e72ca9d4
ckp-koiken <kentaro.koi0710@gmail.com>
2025-11-24 22:12:59 +0900

    add interpretation

--- 432/B_Permute_to_Minimize.cc
+++ 432/B_Permute_to_Minimize.cc
@@ -5,7 +5,11 @@ int main() {
   string X;
   cin >> X;
 
+  // 桁を昇順にソートする
   sort(X.begin(), X.end());
+
+  // 各桁を順番に見て、最初に0でない数値の桁に当たった時
+  // それを先頭の桁と交換し終了する
   int n = (int)X.size();
   for (int i = 0; i < n; i++) {
     if (X[i] > '0') {
