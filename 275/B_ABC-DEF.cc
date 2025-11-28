564799e09198ca804e60496248628645963f4b31
ckp-koiken <kentaro.koi0710@gmail.com>
2025-11-28 14:33:09 +0900

    add interpretation

--- 275/B_ABC-DEF.cc
+++ 275/B_ABC-DEF.cc
@@ -8,13 +8,17 @@ int main() {
 
   int64_t x, y, ans;
 
+  // 剰余計算を応用する
+  // (A * B) % M = ((A % M) * (A % M)) % M
   x = ((A % mod) * (B % mod)) % mod;
   x = (x * (C % mod)) % mod;
   y = ((D % mod) * (E % mod)) % mod;
   y = (y * (F % mod)) % mod;
   
+  // 先に余りを取っているので差を取ると負の数になる可能性がある
+  // 割る数modを足して余りを取ることで解決
   ans = (x + mod - y) % mod;
-  
+
   cout << ans << endl;
   return 0;
 }
