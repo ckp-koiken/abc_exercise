9ac61258c8254b5e65352d667863d8eb004ca4f5
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-08 11:39:44 +0900

    add memo

--- 434/B_Bird_Watching.cc
+++ 434/B_Bird_Watching.cc
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// MEMO:
+// mapを使って解いたが
+// 鳥の大きさの合計を種類ごとに管理する配列と
+// 種類ごとに鳥の数を管理する配列を用意しても良い（種類を添え字として扱う）
+
 int main() {
   int N, M;
   cin >> N >> M;
@@ -20,6 +25,9 @@ int main() {
     double ans = sum / size;
     
     printf("%.10lf\n", ans);
+
+    // 小数の桁数を指定するときは以下のようにしてもOK
+    // cout << fixed << setprecision(10) << ans << endl;
   }
 
   return 0;
