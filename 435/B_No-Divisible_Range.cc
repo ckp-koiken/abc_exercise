c08c2da7a68a4177455cbc77dfaf103b62b47acb
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-09 15:12:26 +0900

    add memo

--- 435/B_No-Divisible_Range.cc
+++ 435/B_No-Divisible_Range.cc
@@ -8,10 +8,14 @@ int main() {
   for (int i = 0; i < N; i++) cin >> A[i];
 
   int ans = 0;
-  for (int l = 0; l < N; l++) {
-    for (int r = 0; r < N; r++) {
-      if (l >= r) continue;
+  for (int l = 0; l < N - 1; l++) {
+    for (int r = l + 1; r < N; r++) {
+      // if (l >= r) continue;
+      // MEMO: lとrをそれぞれ全探索するより
+      // lより大きいrとして探索したほうが早い
 
+      // MEMO: 当初凡ミス
+      // iはrまで参照しなければならないから<=とすべきところを<としていた
       int total = 0;
       for (int i = l; i <= r; i++) {
         total += A[i];
