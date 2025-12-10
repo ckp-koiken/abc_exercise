48d06dcbed49c8dfaaefd023f0408bced25e8aec
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-10 09:27:53 +0900

    add interpretation

--- 271/A_484558.cc
+++ 271/A_484558.cc
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Nを2桁の16進数に変換したいとき
+// N = 16 * a + bを求める
 int main() {
   int N;
   cin >> N;
@@ -9,13 +11,15 @@ int main() {
   a = N / 16;
   b = N % 16;
 
+  // a, bが9以下の場合はそのまま出力
+  // 10以上の場合はA～Fのアルファベットで出力
+  // 'A' + a - 10でaからどの程度離れているかでcharを出力
   if (a <= 9) {
     cout << a;
   } else {
     cout << (char)('A' + a - 10);
   }
 
-  
   if (b <= 9) {
     cout << b;
   } else {
