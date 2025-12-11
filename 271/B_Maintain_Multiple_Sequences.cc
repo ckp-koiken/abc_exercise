62e6a63a4f9d0d2a38edf1bda8b2cb861e9fabf8
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-11 10:04:10 +0900

    add another answer

--- 271/B_Maintain_Multiple_Sequences.cc
+++ 271/B_Maintain_Multiple_Sequences.cc
@@ -17,12 +17,23 @@ int main() {
     }
   }
 
+  // another answer
+  // 配列Aの中にL要素の配列をfor文の中で確保すると無駄なく二次元配列を使える
+  // for (int i = 0; i < N; i++) {
+  //   int L;
+  //   cin >> L;
+  //   A[i] = vector<int>(L);
+  //   for (int j = 0; j < L; j++) {
+  //     cin >> A[i][j];
+  //   }
+  // }
+
   int s, t;
   for (int i = 0; i < Q; i++) {
     cin >> s >> t;
     s--, t--;
     cout << A[s][t] << endl;
   }
-  
+
   return 0;
 }
