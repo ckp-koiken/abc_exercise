37803681043b5081c9b6a1334f1e8cd52c681a22
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-13 17:42:19 +0900

    add B prob

---
+++ 270/B_Hammer.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X, Y, Z;
+  cin >> X >> Y >> Z;
+
+  if (Y < 0) {
+    X = -X;
+    Y = -Y;
+    Z = -Z;
+  }
+
+  if (X < Y) {
+    cout << abs(X) << endl;
+  } else {
+    if (Z > Y) {
+      cout << -1 << endl;
+    } else {
+      cout << abs(Z) + abs(X - Z) << endl;
+    }
+  }
+
+  return 0;
+}
