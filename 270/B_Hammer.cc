9da0b15dd1e61adc709f6e24309891928bd50447
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-13 19:22:57 +0900

    add interpretation

--- 270/B_Hammer.cc
+++ 270/B_Hammer.cc
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Xがゴール、Yが壁、Zがハンマー
+// X < Yなら直接ゴールへ行ける
+// X > Yならゴールへ行くためにハンマーを拾う
+// Z > Yならハンマーを拾うことは不可能
+// Z < YならZにハンマーを拾いに行き、それからXに行く
+
 int main() {
   int X, Y, Z;
   cin >> X >> Y >> Z;
 
+  // Yが負のとき全体に-1をかけて座標Yを正として扱う
   if (Y < 0) {
     X = -X;
     Y = -Y;
@@ -12,11 +19,15 @@ int main() {
   }
 
   if (X < Y) {
+    // X < Yなら直接ゴールへ行くのでXまでの距離を表示
     cout << abs(X) << endl;
   } else {
+    // X > Yのときゴールへ行く前にハンマーを拾う
     if (Z > Y) {
+      // Z > Yのとき拾えないので不可能
       cout << -1 << endl;
     } else {
+      // Z < Yのとき、一回Zに行ってその後Xへ向かう
       cout << abs(Z) + abs(X - Z) << endl;
     }
   }
