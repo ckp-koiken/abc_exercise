aa91cdb84c85f90961fa2e3f2a2a67a65528b2ff
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-15 11:08:29 +0900

    add 269 & solve A prob

---
+++ 269/A_Anyway_Takahashi.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  int ans = (a + b) * (c - d);
+
+  cout << ans << endl;
+  cout << "Takahashi" << endl;
+  return 0;
+}
