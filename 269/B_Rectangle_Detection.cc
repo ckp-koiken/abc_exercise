328f901f55c845a82ca5f4f6b4c65e86628ee8c2
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-15 11:41:30 +0900

    minor fix & add another answer

--- 269/B_Rectangle_Detection.cc
+++ 269/B_Rectangle_Detection.cc
@@ -9,7 +9,6 @@ int main() {
   vector<int> horizon;
   vector<int> vertical;
 
-  vector<vector<int>> ans;
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       if (S[i][j] == '#') {
@@ -25,5 +24,23 @@ int main() {
   D = *max_element(horizon.begin(), horizon.end());
   cout << A << " " << B << endl;
   cout << C << " " << D << endl;
+
+  // another answer
+  // A, B, C, Dを大小比較して更新しつづければ配列を使わないですむ
+  // A = 100, B = -100, C = 100, D = -100;
+
+  // for (int i = 0; i < 10; i++) {
+  //   for (int j = 0; j < 10; j++) {
+  //     if (S[i][j] == '#') {
+  //       A = min(A, i + 1);
+  //       B = max(B, i + 1);
+  //       C = min(C, j + 1);
+  //       D = max(D, j + 1);
+  //     }
+  //   }
+  // }
+  // cout << A << " " << B << endl;
+  // cout << C << " " << D << endl;
+
   return 0;
 }
