14fe0d7cddb788b08b4f5c83065acf2ac723d15e
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-24 18:43:10 +0900

    add interpretation

--- 264/B_Nice_Grid.cc
+++ 264/B_Nice_Grid.cc
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 8行8列のグリッド
+// 中央のマスを中心として対称で規則的な形をしていることを利用する
+// 中央のマスからの距離が奇数のマスは黒、偶数のマスは白
+// チェビシェフ距離（チェス盤距離）が奇数の時黒、そうでないとき白
+// max{|R - 8|, |C - 8|}
 int main() {
   int R, C;
   cin >> R >> C;
@@ -10,6 +15,6 @@ int main() {
   } else {
     cout << "white" << endl;
   }
-  
+
   return 0;
 }
