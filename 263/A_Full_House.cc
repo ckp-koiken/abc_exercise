1908ee487d98d0b26d587e12c732ccab164d34a8
ckp-koiken <kentaro.koi0710@gmail.com>
2025-12-25 22:23:22 +0900

    add another answer

--- 263/A_Full_House.cc
+++ 263/A_Full_House.cc
@@ -21,3 +21,23 @@ int main() {
   cout << "Yes" << endl;
   return 0;
 }
+
+// another answer
+// 整数5個を配列にしてソートし3回、2回出現しているかを確かめれば良い
+// int main() {
+//   vector<int> a(5);
+//   for (int i = 0; i < 5; i++) {
+//     cin >> a[i];
+//   }
+
+//   // 昇順にソート
+//   sort(a.begin(), a.end());
+
+//   // 最初3つとその次の2つの組み合わせか、最初2つとその次の3つの組み合わせかを確認
+//   if ((a[0] == a[2] && a[3] == a[4]) || (a[0] == a[1] && a[2] == a[4])) {
+//     cout << "Yes" << endl;
+//   } else {
+//     cout << "No" << endl;
+//   }
+//     return 0;
+// }
