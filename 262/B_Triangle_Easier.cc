b5db8733a4f25da58018f0341ad51edefcac9caf
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-02 18:58:41 +0900

    add B prob

---
+++ 262/B_Triangle_Easier.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<bool>> adj(n, vector<bool>(n));
+
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    u -= 1; v -= 1;
+    adj[u][v] = adj[v][u] = true;
+  }
+
+  int ans = 0;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      for (int k = j + 1; k < n; k++) {
+        if (adj[i][j] && adj[j][k] && adj[k][i]) {
+          ans++;
+        }
+      }
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
