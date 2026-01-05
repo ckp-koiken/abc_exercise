db28de6b876cc6a2d62b2596f93a2cd581ccf842
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-05 15:55:17 +0900

    add another answer

--- 439/B_Happy_Number.cc
+++ 439/B_Happy_Number.cc
@@ -29,3 +29,50 @@ int main() {
   cout << "No" << endl;
   return 0;
 }
+
+
+// another answer
+// ハッピー数の操作を行う関数を用意して
+// nが1に到達するまで操作後の数値の置き換え続ける
+
+// ハッピー数の操作結果を返す関数
+// int f(int n) {
+//   // 一度文字列にする
+//   string s = to_string(n);
+//
+//   // 各桁の2乗を足していく
+//   int res = 0;
+//   for (auto &nx : s) {
+//     res += (nx - '0') * (nx - '0');
+//   }
+//
+//   return res;
+// }
+//
+// int main() {
+//   int n;
+//   cin >> n;
+//
+//   // ハッピー数の操作結果を管理するsetを用意
+//   set<int> history;
+//
+//   // nが1になるまでハッピー数かどうかの確認を繰り返す
+//   // n>1のとき、historyのなかにnがあればno
+//   // nがhistoryの中にあるということはすでにこのnを見たことがある
+//   // 同じnがでてくるということは無限ループに突入してしまうのでアウト
+//   while (n > 1) {
+//     if (history.count(n)) {
+//       cout << "No" << endl;
+//       return 0;
+//     }
+//
+//     // nをhistoryに追加
+//     history.insert(n);
+//
+//     // nをハッピー数操作した結果に上書き
+//     n = f(n);
+//   }
+//
+//   cout << "Yes" << endl;
+//   return 0;
+// }
