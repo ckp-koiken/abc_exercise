1f824893892fd97476c6a339c42c9cd5ed5b3160
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-11 20:03:48 +0900

    add interpreration

--- 257/B_1D_Pawn.cc
+++ 257/B_1D_Pawn.cc
@@ -5,24 +5,34 @@ int main() {
   int N, K, Q;
   cin >> N >> K >> Q;
 
+  // コマの位置を管理する配列
   vector<int> pos(K);
   for (int i = 0; i < K; i++) {
     cin >> pos[i];
+    // 添え字に合わせる
     pos[i]--;
   }
 
+  // Q回の操作を行う
   while (Q--) {
     int L;
     cin >> L;
+
+    // コマの番号を添え字に合わせる
     int i = L - 1;
 
+    // コマが右端にあるなら何もしない
     if (pos[i] == N - 1) continue;
 
+    // 右隣にコマがいないなら動ける
+    // iが最も右のコマであるとき
+    // またはpos[i]のコマが隣に動いたときpos[i+1]にぶつからなければ動ける
     if (i == K - 1 || pos[i] + 1 < pos[i + 1]) {
       pos[i]++;
     }
   }
 
+  // コマの位置を表示
   for (int i = 0; i < K; i++) {
     cout << pos[i] + 1 << ' ';
   }
