b0ffbf2b70fdeed8a5260faec4aa35366234e8cf
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-12 22:38:24 +0900

    add another answer

--- 440/B_Trifecta.cc
+++ 440/B_Trifecta.cc
@@ -21,3 +21,29 @@ int main() {
   cout << '\n';
   return 0;
 }
+
+// another answer
+// pair<int, int>のvectorを使えばもう少しシンプルに書ける
+// int main() {
+//   int n;
+//   cin >> n;
+//
+//   // タイムと番号をpairで管理する配列を作成
+//   vector<pair<int, int>> ti;
+//   for (int i = 0; i < n; i++) {
+//     int t;
+//     cin >> t;
+//     // ゴールまでのタイムと番号をpairで追加
+//     ti.push_back({t, i + 1});
+//   }
+//
+//   // タイム(ti.first: t)順にソート
+//   sort(ti.begin(), ti.end());
+//
+//   // 順位(ti.second)を上から3位分表示
+//   for (int i = 0; i < 3; i++) {
+//     cout << ti[i].second << ' ';
+//   }
+//   cout << '\n';
+//   return 0;
+// }
