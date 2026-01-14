a4ae6f3640af45b35daa2f00e4bf07022e2310ac
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-14 22:22:25 +0900

    add 255 & solve A prob

---
+++ 255/A_You_should_output_ARC_though_this_is_ABC.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int R, C;
+  cin >> R >> C;
+  vector<vector<int>> A(2, vector<int>(2));
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      cin >> A[i][j];
+    }
+  }
+
+  cout << A[R - 1][C - 1] << endl;
+  return 0;
+}
