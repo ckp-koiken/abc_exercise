ad00316746370d6213d43e3ec38bb7da71021057
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-15 20:56:52 +0900

    add another answer

--- 254/A_Last_Two_Digits.cc
+++ 254/A_Last_Two_Digits.cc
@@ -8,5 +8,13 @@ int main() {
   int m = (int)N.size();
 
   cout << N[m - 2] << N[m - 1] << endl;
+
+  // another answer
+  // 1の位は10で割った余り
+  // 10の位は100で割った余りを10で割った商
+  // 100で割った余りだと304などを4で出力してしまうので注意
+  // int n;
+  // cin >> n;
+  // cout << (n % 100) / 10 << n % 10 << endl;
   return 0;
 }
