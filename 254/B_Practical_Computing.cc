d4474808b5922a1ab290e79cbdcd8acd8e73662d
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-16 18:10:40 +0900

    add B prob

---
+++ 254/B_Practical_Computing.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<vector<int>> A(N, vector<int>(N, 0));
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j <= i; j++) {
+      if (j == 0 || j == i) {
+        A[i][j] = 1;
+      } else {
+        A[i][j] = A[i - 1][j - 1] + A[i - 1][j];
+      }
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j <= i; j++) {
+      cout << A[i][j] << (j == i ? '\n' : ' ');
+    }
+  }
+
+  return 0;
+}
