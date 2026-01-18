fbf5acd1d0cf5ae702f72751282272b77e5d8939
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-18 17:33:42 +0900

    add interpretation

--- 441/A_Black_Square.cc
+++ 441/A_Black_Square.cc
@@ -8,6 +8,8 @@ int main() {
   int rev_P = P + 100;
   int rev_Q = Q + 100;
 
+  // X <= rev_P, Y <= rev_Qではないことに注意
+  // Pから100マス、Qから100マス塗られているので端は含まない
   if ((X >= P && X < rev_P) && (Y >= Q && Y < rev_Q)) {
     cout << "Yes" << endl;
   } else {
