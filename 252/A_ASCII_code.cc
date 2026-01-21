2de9717fd30117cfe71ac3896c3a6ffc43d6acd2
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-22 00:05:34 +0900

    add interpretation & another answer

--- 252/A_ASCII_code.cc
+++ 252/A_ASCII_code.cc
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 整数で読んでchar型で出力する
 int main() {
   int N;
   cin >> N;
 
   cout << (char)N << endl;
+
+  // another answer
+  // printfで表示してもよい
+  // int n;
+  // scanf("%d", &n);
+  // printf("%c\n", n);
   return 0;
 }
