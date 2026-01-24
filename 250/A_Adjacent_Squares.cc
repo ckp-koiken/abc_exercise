c3a506d48ec5d961d7b8958419d748bf39888563
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-24 22:24:52 +0900

    add another answer

--- 250/A_Adjacent_Squares.cc
+++ 250/A_Adjacent_Squares.cc
@@ -18,3 +18,22 @@ int main() {
   
   return 0;
 }
+
+// another answer
+// R, Cの左側にマスが存在する条件はC != 1
+// R, Cの右側にマスが存在することは C != W
+// 上下も同様
+// int main() {
+//   int h, w, r, c;
+//   cin >> h >> w >> r >> c;
+//
+//   int ans = 0;
+//   if (c != 1) ans++;
+//   if (c != w) ans++;
+//   if (r != 1) ans++;
+//   if (r != h) ans++;
+//
+//   cout << ans << endl;
+//
+//   return 0;
+// }
