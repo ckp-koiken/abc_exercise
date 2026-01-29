3cf0cfe99e701b82cd19e327e544fe3732b3a469
ckp-koiken <kentaro.koi0710@gmail.com>
2026-01-29 19:33:19 +0900

    fix typo

--- 248/B_Slimes.cc
+++ 248/B_Slimes.cc
@@ -17,8 +17,8 @@ int main() {
   }
 
   // another answer
-  // 無限ループにしなくてもwhileの条件をA >= Bにしてもよい
-  // while (A >= B) {
+  // 無限ループにしなくてもwhileの条件をA < Bにしてもよい
+  // while (A < B) {
   //   A *= K;
   //   cnt++;
   // }
