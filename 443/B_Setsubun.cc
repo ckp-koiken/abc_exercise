138765608577f5b3f7b9df55929af24fb9334176
ckp-koiken <kentaro.koi0710@gmail.com>
2026-02-01 21:03:37 +0900

    add another answer

--- 443/B_Setsubun.cc
+++ 443/B_Setsubun.cc
@@ -12,7 +12,21 @@ int main() {
     cnt += N;
     year++;
   }
-
   cout << year << endl;
+
+  // another answer
+  // while文を無限ループさせてcntがKを超えた段階で答えを出すのでもよい
+  // int sum = 0;
+  // int year = 0;
+  // while (true) {
+  //   sum += N;
+  //   if (sum >= K) {
+  //     cout << year << endl;
+  //     break;
+  //   }
+  //   N++;
+  //   year++;
+  // }
+
   return 0;
 }
