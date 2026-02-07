50c443229dbc73af20088cd95e313dfc4e8d01dc
ckp-koiken <kentaro.koi0710@gmail.com>
2026-02-07 18:42:14 +0900

    add another answer

--- 243/B_Hit_and_Blow.cc
+++ 243/B_Hit_and_Blow.cc
@@ -24,6 +24,23 @@ int main() {
     }
   }
 
+  // another answer
+  // Bの要素とindexを連想配列で記録させればもっと高速に解ける
+
+  // Bの各要素が何番目(index)にあるのかを管理するmapを用意
+  // map<int, int> ma_b;
+  // for (int i = 0; i < N; i++) ma_b[B[i]] = i;
+
+  // for (int i = 0; i < N; i++) {
+  //   // ma_bにAの要素が存在して
+  //   if (ma_b.count(A[i])) {
+  //     // その要素がiではない（同じindexではない）ときにans2を増やす
+  //     if (ma_b[A[i]] != i) {
+  //       ans2++;
+  //     }
+  //   }
+  // }
+
   cout << ans1 << endl;
   cout << ans2 << endl;
   return 0;
