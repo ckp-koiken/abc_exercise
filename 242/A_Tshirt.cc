39c7b6f9888118b7ca612ad4457719cf4aad8dd3
ckp-koiken <kentaro.koi0710@gmail.com>
2026-02-10 14:35:02 +0900

    add 242 & solve A prob

---
+++ 242/A_Tshirt.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B, C, X;
+  cin >> A >> B >> C >> X;
+
+  double ans = 0;
+  if (X >= A + 1 && X <= B) {
+    double div = B - A;
+    ans = C / div;
+  } else if (X <= A) {
+    ans = 1;
+  } else if (X > B) {
+    ans = 0;
+  }
+
+  printf("%.10f\n", ans);
+  return 0;
+}
