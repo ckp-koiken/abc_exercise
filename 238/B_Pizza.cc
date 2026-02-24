094a29ad0133869141a5c3fea4f4d9548675a548
ckp-koiken <kentaro.koi0710@gmail.com>
2026-02-24 15:02:40 +0900

    add B prob

---
+++ 238/B_Pizza.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  
+  vector<int> cut;
+  cut.push_back(0);
+  int sum = 0;
+  
+  for (int i = 0; i < n; i++) {
+    sum += a[i];
+    cut.push_back(sum % 360);
+  }
+  
+  cut.push_back(360);
+  sort(cut.begin(), cut.end());
+  int ans = 0;
+  
+  for (int i = 0; i <= n; i++) {
+    int now = cut[i + 1] - cut[i];
+    ans = max(ans, now);
+  }
+  
+  cout << ans << endl;
+  return 0;
+}
