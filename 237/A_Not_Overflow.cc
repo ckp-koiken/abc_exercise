dc628ccb614358f90f57682b7db93efda5b65c6b
ckp-koiken <kentaro.koi0710@gmail.com>
2026-02-25 20:03:52 +0900

    add another answer

--- 237/A_Not_Overflow.cc
+++ 237/A_Not_Overflow.cc
@@ -8,6 +8,11 @@ int main() {
   int64_t low = -1 * pow(2, 31);
   int64_t high = pow(2, 31);
   
+  // another answer
+  // 1を31回シフトするのでもよい
+  // int64_t low = -1 * (int64_t)1 << 31;
+  // int64_t high = (int64_t)1 << 31;
+  
   if (N >= low && N < high) {
     cout << "Yes" << endl;
   } else {
