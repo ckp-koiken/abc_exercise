6f898630b6dcda78c85570a30ea54764a6dfc464
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-01 17:29:58 +0900

    add 447 & add A prob

---
+++ 447/A_Seats2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  if ((N + 1) / 2 >= M) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  
+  return 0;
+}
