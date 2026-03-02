3fc975736227f285001ad750cc39eecc35176788
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-02 19:26:15 +0900

    add another answer

--- 447/B_mpp.cc
+++ 447/B_mpp.cc
@@ -29,3 +29,31 @@ int main() {
   cout << '\n';
   return 0;
 }
+
+// another answer
+// アルファベット26文字ごとに何回登場したかを数える配列を用意する
+// 最大値を*max_element()で取得してそれ以外のアルファベットを表示すれば良い
+
+// int main() {
+//   string s;
+//   cin >> s;
+//   // 26文字それぞれのアルファベットの登場回数を数えるための配列
+//   vector<int> cnt(26);
+
+//   // 添え字の指定をc-'a'とすることで
+//   // アルファベットを'a'からの距離で管理（0〜25まで）
+//   for (char c : s) {
+//     cnt[c - 'a']++;
+//   }
+
+//   // 最大値を取得
+//   int ma = *max_element(cnt.begin(), cnt.end());
+
+//   // 空の文字列tに最大値以外の文字を追加
+//   string t = "";
+//   for (char c : s) {
+//     if (cnt[c - 'a'] != ma) t += c;
+//   }
+
+//   cout << t << endl;
+// }
