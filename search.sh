9551078b5c16fad8debe151d200aaa78e0f89c2d
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-03 09:17:21 +0900

    add parameter

--- search.sh
+++ search.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
+
+# コマンドライン引数でA,B,C...を渡すとその問題が存在しないディレクトリを探してくれる
 for d in */; do
-  if ! find "$d" -maxdepth 1 -type f -name 'C_*' | grep -q .; then
+  if ! find "$d" -maxdepth 1 -type f -name $1'_*' | grep -q .; then
     echo "$d"
   fi
 done
-
-# for d in */; do
-#   if ! find "$d" -maxdepth 1 -type f -name 'B_*' | grep -q .; then
-#     echo "$d"
-#   fi
-# done
