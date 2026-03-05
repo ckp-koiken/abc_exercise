74638c0d37b581d6449cf1a2e1e5241e717cd229
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-05 11:48:33 +0900

    add another answer

--- 308/B_Default_Price.cc
+++ 308/B_Default_Price.cc
@@ -23,6 +23,18 @@ int main() {
     }
   }
 
+  // another answer
+  // priceという価格を保持する変数を用意しておいて初期値をP[0]にしておく
+  // C[i]でD[j]を見ていって見つかったら対応する価格P[j]に上書きする
+  // 見つからなかったらそのままP[0]を足す
+  // for (int i = 1; i <= N; i++) {
+  //   int price = P[0];
+  //   for (int j = 1; j <= M; j++) {
+  //     if (C[i] == D[j]) price = P[j];
+  //   }
+  //   total += price;
+  // }
+
   cout << total << endl;
   return 0;
 }
