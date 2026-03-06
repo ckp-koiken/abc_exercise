8e656c9ceb4e647b246db5ed4cd99f01f0de3e30
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-07 00:31:29 +0900

    add another answer

--- 236/B_Who_is_missing.cc
+++ 236/B_Who_is_missing.cc
@@ -17,5 +17,20 @@ int main() {
       cout << m.first << endl;
     }
   }
+
+  // another answer
+  // mapを使わずにvectorで数えても良い
+  // vector<int> count(N + 1);
+  // for (int i = 0; i < 4 * N - 1; i++) {
+  //   int a;
+  //   cin >> a;
+  //   count[a]++;
+  // }
+
+  // for (int i = 1; i <= N; i++) {
+  //   if (count[i] == 3) {
+  //     cout << i << endl;
+  //   }
+  // }
   return 0;
 }
