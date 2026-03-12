54b9e495a7c97fc3b0f85e7b85d338bd0b846e24
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-12 10:50:28 +0900

    add memo

--- 245/B_Mex.cc
+++ 245/B_Mex.cc
@@ -12,6 +12,9 @@ int main() {
   int max_num = *max_element(A.begin(), A.end());
   set<int> se(A.begin(), A.end());
 
+  // 配列Aの中にmax_numまでの数字が欠けずに全てあった場合
+  // 配列の中だけで答えを見つけることができない
+  // なのでmax_num+1まで探索する（配列内の最大値+1が答えになる）
   for (int i = 0; i <= max_num + 1; i++) {
     if (!se.count(i)) {
       cout << i << endl;
