6ce2ab1ac18e35b9db552a564de5ad8cae1c7e6b
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-15 12:53:11 +0900

    add 449 & solve A prob

---
+++ 449/A_Pi.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double PI = acos(-1);
+
+int main() {
+  double D;
+  cin >> D;
+
+  double ans = PI * (D / 2) * (D / 2);
+
+  cout << fixed << setprecision(10) << ans << endl;
+  return 0;
+}
