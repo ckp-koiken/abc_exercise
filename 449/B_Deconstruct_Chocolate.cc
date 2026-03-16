9bd4c7100a9e284b055a1531c865f3f58b7b8aaf
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-16 15:22:58 +0900

    add interpretation

--- 449/B_Deconstruct_Chocolate.cc
+++ 449/B_Deconstruct_Chocolate.cc
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 二次元配列を使わなくてよく、単純にチョコレートを食べた部分の面積を求めればいい
+// 複雑に考えなくて良い
+// タイプ1の場合は下R行なくなるのでHからその分を引く
+// タイプ2の場合は右C列なくなるのでWからその分を引く
 int main() {
   int H, W, Q;
   cin >> H >> W >> Q;
   
   for (int qi = 0; qi < Q; qi++) {
+    // RとCまとめてXで受け取る
     int type, X;
     cin >> type >> X;
 
     if (type == 1) {
       cout << X * W << endl;
+      // Hから下X行なくす
       H -= X;
     }
 
     if (type == 2) {
       cout << X * H << endl;
+      // Wから右X列なくす
       W -= X;
     }
   }
