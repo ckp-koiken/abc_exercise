c187c87c7870fe217478e57bd7bad0f7edc908d5
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-17 11:52:39 +0900

    add another answer

--- 290/B_Qual_B.cc
+++ 290/B_Qual_B.cc
@@ -20,5 +20,25 @@ int main() {
   }
 
   cout << '\n';
+
+  // another answer
+  // 空の文字列tを別途用意して条件通りに文字列を作り直しても良い
+  /*
+  string t;
+  int cnt = 0;
+  for (int i = 0; i < N; i++) {
+    if (S[i] == 'x') {
+      t += 'x';
+    } else {
+      if (cnt < K) {
+        t += 'o';
+        cnt++;
+      } else {
+        t += 'x';
+      }
+    }
+  }
+  cout << t << endl;
+  */
   return 0;
 }
