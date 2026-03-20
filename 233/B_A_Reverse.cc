3db3124faa76843ad5ba73d2b0829326aafeda51
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-20 19:23:00 +0900

    add MEMO

--- 233/B_A_Reverse.cc
+++ 233/B_A_Reverse.cc
@@ -6,6 +6,9 @@ int main() {
   string S;
   cin >> L >> R >> S;
 
+  // MEMO: イテレータは半開区感で指定する
+  // reverse(first, last)なら、firstは含むがlastは含まない
+  // なのでlast部分はS.begin()+Rとして1多く指定している
   reverse(S.begin() + L - 1, S.begin() + R);
 
   cout << S << endl;
