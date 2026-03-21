b706a117a03ea7da27815044ce78f060e66fd27a
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-21 19:48:26 +0900

    refactoring

--- 284/B_Multi_Test_Cases.cc
+++ 284/B_Multi_Test_Cases.cc
@@ -1,19 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// MEMO: マルチテストケースではテストケースごとに問題を解く部分を関数にすると良い
+void solve() {
+  int N;
+  cin >> N;
+  int ans = 0;
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    if (a % 2 == 1) ans++;
+  }
+  cout << ans << endl;
+}
+
 int main() {
   int T;
   cin >> T;
   for (int i = 0; i < T; i++) {
-    int N;
-    cin >> N;
-    int ans = 0;
-    for (int i = 0; i < N; i++) {
-      int a;
-      cin >> a;
-      if (a % 2 == 1) ans++;
-    }
-    cout << ans << endl;
+    solve();
   }
   return 0;
 }
