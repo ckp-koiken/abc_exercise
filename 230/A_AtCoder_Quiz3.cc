1846c4451ab4d68830d9cb0c76b07ecdb4f6bf44
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-26 17:09:02 +0900

    add another answer

--- 230/A_AtCoder_Quiz3.cc
+++ 230/A_AtCoder_Quiz3.cc
@@ -11,5 +11,10 @@ int main() {
   } else {
     cout << "AGC00" << N << endl;
   }
+
+  // another answer
+  // 3桁で表示して穴埋めにはsetwとsetfillを使う
+  // cのprintfでも似たような桁埋めができる
+  // cout << "AGC" << setfill('0') << setw(3) << N << endl;
   return 0;
 }
