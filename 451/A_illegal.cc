3c4ed71b7832832439ca02780f5666ecb2e77213
ckp-koiken <kentaro.koi0710@gmail.com>
2026-03-29 20:32:10 +0900

    add 451 & solve A prob

---
+++ 451/A_illegal.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  int n = (int)S.size();
+
+  if (n % 5 == 0) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  
+  return 0;
+}
