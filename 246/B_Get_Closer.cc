87c41ef29e28243bb2d6d1d891ff94a8be914ff7
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-01 10:16:59 +0900

    add interpretation

--- 246/B_Get_Closer.cc
+++ 246/B_Get_Closer.cc
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// A, Bのユークリッド距離はsqrt{A^2 + B^2}
+// 距離が1になるようなA, Bを選べば良い
+// A/sqrt{A^2 + B^2}、B/sqrt{A^2 + B^2}を計算する
 int main() {
   int A, B;
   cin >> A >> B;
