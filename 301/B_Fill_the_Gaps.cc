f715d408de3071851df17839241d015baa6c3736
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-02 09:54:51 +0900

    solve B prob

---
+++ 301/B_Fill_the_Gaps.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) cin >> A[i];
+
+  vector<int> B;
+  for (int i = 0; i < N - 1; i++) {
+    B.push_back(A[i]);
+    if (abs(A[i] - A[i + 1]) > 1) {
+      if (A[i] < A[i + 1]) {
+        int diff = A[i + 1] - A[i];
+        for (int j = 1; j < diff; j++) {
+          B.push_back(A[i] + j);
+        }
+      } else {
+        int diff = A[i] - A[i + 1];
+        for (int j = 1; j < diff; j++) {
+          B.push_back(A[i] - j);
+        }
+      }
+    }
+  }
+
+  B.push_back(A[N - 1]);
+
+  for (int b : B) {
+    cout << b << ' ';
+  }
+  cout << '\n';
+
+  return 0;
+}
