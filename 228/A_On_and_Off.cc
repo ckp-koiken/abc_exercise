ad9cfc0cb971f37b89f0c9dca47426a9c586f311
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-04 00:17:13 +0900

    add A prob

--- 228/A_On_and_Off.cc
+++ 228/A_On_and_Off.cc
@@ -5,13 +5,17 @@ int main() {
   int S, T, X;
   cin >> S >> T >> X;
 
+  // SよりTが大きい時
   if (S < T) {
+    // XがSとTの間にあれば電気がついている
     if (S <= X && X < T) {
       cout << "Yes" << endl;
     } else {
       cout << "No" << endl;
     }
   } else {
+    // TよりSが大きい時
+    // XがTより前か、S以上であれば電気がついている
     if (X < T || S <= X) {
       cout << "Yes" << endl;
     } else {
