1c17bd82cc4c826c8a62fba23906f482760a3a05
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-08 10:16:15 +0900

    add 227 & add A prob

---
+++ 227/A_Last_Card.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, K, A;
+  cin >> N >> K >> A;
+
+  int ans = (A + K - 1) % N;
+  if (ans == 0) ans = N;
+
+  cout << ans << endl;
+  return 0;
+}
