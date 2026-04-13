3d003ce2fd1cd453ae0e7eba1a9cbd6ba4b6e6e3
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-13 15:57:05 +0900

    add B prob

---
+++ 364/B_Grid_Walk.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int h, w, si, sj;
+  cin >> h >> w >> si >> sj;
+  vector<string> C(h);
+  for (int i = 0; i < h; i++) cin >> C[i];
+  string x;
+  cin >> x;
+
+  si--, sj--;
+
+  for (char xx : x) {
+    if (xx == 'L') {
+      int ti = si, tj = sj - 1;
+      if (tj >= 0 && C[ti][tj] != '#') {
+        sj--;
+      }
+    }
+    if (xx == 'R') {
+      int ti = si, tj = sj + 1;
+      if (tj < w && C[ti][tj] != '#') {
+        sj++;
+      }
+    }
+    if (xx == 'U') {
+      int ti = si - 1, tj = sj;
+      if (ti >= 0 && C[ti][tj] != '#') {
+        si--;
+      }
+    }
+    if (xx == 'D') {
+      int ti = si + 1, tj = sj;
+      if (ti < h && C[ti][tj] != '#') {
+        si++;
+      }
+    }
+  }
+
+  si++, sj++;
+
+  cout << si << ' ' << sj << endl;
+
+  return 0;
+}
