d8478d0b5b1e119a6875bbfaa42bbcb405a513a6
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-14 09:52:11 +0900

    solve B prob

---
+++ 376/B_Cut0.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double X;
+  cin >> X;
+
+  int xx = X * 1000;
+
+  double ans = xx / 1000.0;
+
+  cout << ans << endl;
+  return 0;
+}
