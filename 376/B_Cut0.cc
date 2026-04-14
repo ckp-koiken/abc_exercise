06d44555d3f657359ac0e6af9f3e61b1d10f0ae3
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-14 09:54:42 +0900

    add another answer

--- 376/B_Cut0.cc
+++ 376/B_Cut0.cc
@@ -12,3 +12,15 @@ int main() {
   cout << ans << endl;
   return 0;
 }
+
+// another answer
+// C++の場合実数を受け取ってそのままcoutすれば勝手に0が消える
+// 過剰な0を出力しない仕組みになっている
+
+// int main() {
+//   double x;
+//   cin >> x;
+
+//   cout << x << endl;
+//   return 0;
+// }
