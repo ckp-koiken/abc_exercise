afd4026a2f494f9cd815de2f97b6126d708877c5
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-15 17:01:31 +0900

    add B prob

---
+++ 250/B_Enlarged_Checker_Board.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, a, b;
+  cin >> n >> a >> b;
+  vector<string> s(n * a, string(n * b, '.'));
+
+  for (int i = 0; i < n * a; i++) {
+    for (int j = 0; j < n * b; j++) {
+      int r = i / a, c = j / b;
+      if ((r + c) % 2 == 1) s[i][j] = '#';
+    }
+  }
+
+  for (int i = 0; i < n * a; i++) {
+    cout << s[i] << endl;
+  }
+
+  return 0;
+}
