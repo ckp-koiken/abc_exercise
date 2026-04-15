1ee5c7b9a2b6d33751b4e3a5822d963b95d44e76
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-15 17:05:16 +0900

    add interpretation

--- 250/B_Enlarged_Checker_Board.cc
+++ 250/B_Enlarged_Checker_Board.cc
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// n * nマスの市松模様（白黒塗分け）をa * b倍したものと考える
+// 大きい(n*a)*(n*b)マスをそれぞれaとbで割る
+// マス番号の合計値の偶奇を判定し奇数の時に黒く塗ればよい
 int main() {
   int n, a, b;
   cin >> n >> a >> b;
@@ -8,7 +11,11 @@ int main() {
 
   for (int i = 0; i < n * a; i++) {
     for (int j = 0; j < n * b; j++) {
+      // 大きなマスの番号iとjをそれぞれaとbで割って小さいマスの番号だと考える
       int r = i / a, c = j / b;
+
+      // マス番号の縦横を合計し偶奇を判定
+      // 奇数の時に黒く塗る
       if ((r + c) % 2 == 1) s[i][j] = '#';
     }
   }
