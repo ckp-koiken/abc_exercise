afd25bfac36d7786ce87666c02b85b8074670365
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-16 18:52:55 +0900

    add B prob

---
+++ 366/B_Vertical_Writing.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> s(n);
+  for (int i = 0; i < n; i++) {
+    cin >> s[i];
+  }
+
+  int m = 0;
+  for (int i = 0; i < n; i++) {
+    m = max(m, (int)s[i].size());
+  }
+
+  vector<string> t(m, string(n, '*'));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < (int)s[i].size(); j++) {
+      t[j][n - 1 - i] = s[i][j];
+    }
+  }
+
+  for (int i = 0; i < m; i++) {
+    while (t[i].back() == '*') t[i].pop_back();
+    cout << t[i] << endl;
+  }
+  return 0;
+}
