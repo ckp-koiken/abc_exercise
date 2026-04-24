9135f9c2c2a3d36d888df36605f4a9214b44177a
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-24 14:53:27 +0900

    add B prob python

---
+++ 454/B.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+F = {int(x) for x in input().split()}
+
+if len(F) == N:
+    print("Yes")
+else:
+    print("No")
+
+if len(F) == M:
+    print("Yes")
+else:
+    print("No")
