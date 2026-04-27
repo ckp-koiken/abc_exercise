6dbc77763d5afaea9c40cbe8eae24c5455fc3a67
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-27 22:01:57 +0900

    add another answer

--- 225/A_Distinct_Strings.cc
+++ 225/A_Distinct_Strings.cc
@@ -13,5 +13,20 @@ int main() {
   } while (next_permutation(S.begin(), S.end()));
 
   cout << (int)se.size() << endl;
+
+  // another answer
+  // 3文字すべて同じ場合は1通り
+  // 3文字すべて相異なる文字の場合は3! = 6通り
+  // 3文字のうち2文字が同じで残り1文字が別の文字で構成される場合3通り
+  // set<char> sec(S.begin(), S.end());
+  // int m = (int)sec.size();
+
+  // if (m == 1) {
+  //   cout << 1 << endl;
+  // } else if (m == 3) {
+  //   cout << 6 << endl;
+  // } else {
+  //   cout << 3 << endl;
+  // }
   return 0;
 }
