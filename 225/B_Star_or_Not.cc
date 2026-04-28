78d8cc708ed84ee35100481b3850d21a3b13b75b
ckp-koiken <kentaro.koi0710@gmail.com>
2026-04-28 22:38:06 +0900

    add another answer

--- 225/B_Star_or_Not.cc
+++ 225/B_Star_or_Not.cc
@@ -25,5 +25,28 @@ int main() {
 
   cout << "No" << endl;
 
+  // another answer
+  // ある頂点から別の頂点に辺が出ている数だけを数えれば良い
+  // 頂点ごとに出ている辺の数を数える一次元のvectorで良い
+  // （↑のmapは辺が出ている元の頂点とどこの頂点に伸びているかを全部数えている）
+  // （そこまで必要ない）
+
+  // vector<int> deg(N + 1);
+  // for (int i = 0; i < N - 1; i++) {
+  //   int a, b;
+  //   cin >> a >> b;
+  //   deg[a]++;
+  //   deg[b]++;
+  // }
+
+  // for (int i = 1; i <= N; i++) {
+  //   if (deg[i] == N - 1) {
+  //     cout << "Yes" << endl;
+  //     return 0;
+  //   }
+  // }
+
+  // cout << "No" << endl;
+
   return 0;
 }
