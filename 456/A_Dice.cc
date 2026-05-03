bc1fd29cda1b5ae926dd399a5f7d61a279f32db4
ckp-koiken <kentaro.koi0710@gmail.com>
2026-05-03 17:32:19 +0900

    add 456 & solve A prob

---
+++ 456/A_Dice.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X;
+  cin >> X;
+
+  if (X < 3 || X > 18) {
+    cout << "No" << endl;
+  } else {
+    cout << "Yes" << endl;
+  }
+
+  return 0;
+}
