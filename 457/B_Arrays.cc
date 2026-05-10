86dfda37b84ace5382b202fb5c9e7af6cca1ee47
ckp-koiken <kentaro.koi0710@gmail.com>
2026-05-10 16:19:41 +0900

    add memo

--- 457/B_Arrays.cc
+++ 457/B_Arrays.cc
@@ -15,6 +15,17 @@ int main() {
     grid[i] = A;
   }
 
+  // memo: 可変長配列を二次元配列で読み込むとき
+  // .resize()で必要な長さを確保することができる
+  // for (int i = 0; i < N; i++) {
+  //   int L;
+  //   cin >> L;
+  //   grid[i].resize(L);
+  //   for (int j = 0; j < L; j++) {
+  //     cin >> grid[i][j];
+  //   }
+  // }
+
   cin >> X >> Y;
 
   cout << grid[X - 1][Y - 1] << endl;
