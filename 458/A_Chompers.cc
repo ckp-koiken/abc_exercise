babb78a8f321c8d467ce9a8cccb5db04e0965c28
ckp-koiken <kentaro.koi0710@gmail.com>
2026-05-17 22:58:28 +0900

    add another answer

--- 458/A_Chompers.cc
+++ 458/A_Chompers.cc
@@ -13,5 +13,12 @@ int main() {
   }
 
   cout << '\n';
+
+  // another answer
+  // 最初n文字と最後n文字を除いた部分文字列を抽出しても良い
+  // substr(i, x)でi文字目から長さxの範囲を取得
+  // string ans = S.substr(N, (int)S.size() - N * 2);
+  // cout << ans << endl;
+  
   return 0;
 }
