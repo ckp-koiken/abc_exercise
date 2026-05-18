9dc243eae30ddf13dffd839c190f5c5109804f38
ckp-koiken <kentaro.koi0710@gmail.com>
2026-05-18 14:32:16 +0900

    add another answer

--- 458/B_Count_Adjacent_Cells.cc
+++ 458/B_Count_Adjacent_Cells.cc
@@ -25,3 +25,35 @@ int main() {
 
   return 0;
 }
+
+// another answer
+// 上下左右を見てマスの端でなければ1を足していく
+// int main() {
+//   int h, w;
+//   cin >> h >> w;
+//   vector<vector<int>> ans(h, vector<int>(w));
+
+//   for (int i = 0; i < h; i++) {
+//     for (int j = 0; j < w; j++) {
+//       // 4方向を見る
+
+//       // 左を見て0以上であれば1加える
+//       if (i - 1 >= 0) ans[i][j]++;
+//       // 右をみて右端でなければ（<h)1加える
+//       if (i + 1 < h) ans[i][j]++;
+//       // 上を見て0以上であれば1加える
+//       if (j - 1 >= 0) ans[i][j]++;
+//       // 下を見て最下部でなければ(<w)1加える
+//       if (j + 1 < w) ans[i][j]++;
+//     }
+//   }
+
+//   for (int i = 0; i < h; i++) {
+//     for (int j = 0; j < w; j++) {
+//       cout << ans[i][j] << ' ';
+//     }
+//     cout << '\n';
+//   }
+
+//   return 0;
+// }
