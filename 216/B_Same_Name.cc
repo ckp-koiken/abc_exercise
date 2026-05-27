1b70afb605b52d1086ee1071eeef3bce19d8faca
ckp-koiken <kentaro.koi0710@gmail.com>
2026-05-27 15:56:41 +0900

    add another answer

--- 216/B_Same_Name.cc
+++ 216/B_Same_Name.cc
@@ -20,3 +20,21 @@ int main() {
   }
   return 0;
 }
+
+// another answer
+// 単純に姓と名が一致しているかどうかを全探索してもよい
+// int main() {
+//   int n;
+//   cin >> n;
+//   vector<string> s(n), t(n);
+
+//   string ans = "No";
+//   for (int i = 0; i < n; i++) {
+//     cin >> s[i] >> t[i];
+//     for (int j = 0; j < i; j++) {
+//       if (s[i] == s[j] && t[i] == t[j]) ans = "Yes";
+//     }
+//   }
+
+//   cout << ans << endl;
+// }
