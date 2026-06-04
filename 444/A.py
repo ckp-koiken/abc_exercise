39cbcec6784e704820570edc2130868127413532
ckp-koiken <kentaro.koi0710@gmail.com>
2026-06-04 20:02:20 +0900

    add another answer

--- 444/A.py
+++ 444/A.py
@@ -10,3 +10,11 @@ if N100 == N10 and N10 == N1:
     print("Yes")
 else:
     print("No")
+
+# another answer
+# 文字列で受け取れば添え字で参照できる
+# N = input()
+# if N[0] == N[1] and N[1] == N[2]:
+#     print("Yes")
+# else:
+#     print("No")
