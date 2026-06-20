6197e0776acac712b3a4dbf23e34681c9a0f5c3d
ckp-koiken <kentaro.koi0710@gmail.com>
2026-06-20 22:44:37 +0900

    add another answer

--- 211/B_Cycle_Hit.cc
+++ 211/B_Cycle_Hit.cc
@@ -18,5 +18,22 @@ int main() {
   }
 
   cout << "Yes" << endl;
+
+  // another answer
+  // 配列を利用してi,jが異なるSi=Sjが存在することをNoと判定しても良い
+  // vector<string> S(4);
+  // for (int i = 0; i < 4; i++) cin >> S[i];
+
+  // // 配列を2重ループで見ていく
+  // for (int i = 0; i < 4; i++) {
+  //   for (int j = 0; j < 4; j++) {
+  //     // 添え字が違うのに同じ文字列があればどれかが被っている
+  //     if (i != j && S[i] == S[j]) {
+  //       cout << "No" << endl;
+  //       return 0;
+  //     }
+  //   }
+  // }
+  // cout << "Yes" << endl;
   return 0;
 }
