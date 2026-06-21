134b6577881f6f03683b44af25c87e90823d8695
ckp-koiken <kentaro.koi0710@gmail.com>
2026-06-21 15:38:52 +0900

    add right answer

--- 463/A_169.cc
+++ 463/A_169.cc
@@ -1,14 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int X, Y;
-  cin >> X >> Y;
+// ACが出たがこれは間違い
+// int main() {
+//   int X, Y;
+//   cin >> X >> Y;
 
-  if (X % 16 == 0 && Y % 9 == 0) {
+//   if (X % 16 == 0 && Y % 9 == 0) {
+//     cout << "Yes" << endl;
+//   } else {
+//     cout << "No" << endl;
+//   }
+//   return 0;
+// }
+
+int main() {
+  int x, y;
+  cin >> x >> y;
+
+  // x:y = 16:9は9x = 16yということ
+  if (x * 9 == y * 16) {
     cout << "Yes" << endl;
   } else {
     cout << "No" << endl;
   }
+
   return 0;
 }
