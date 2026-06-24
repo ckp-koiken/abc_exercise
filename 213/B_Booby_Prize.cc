1d69c775b3aeb867da2fc008655ce171abbb02fa
ckp-koiken <kentaro.koi0710@gmail.com>
2026-06-24 17:15:16 +0900

    add another answer

--- 213/B_Booby_Prize.cc
+++ 213/B_Booby_Prize.cc
@@ -17,5 +17,26 @@ int main() {
       cout << i + 1 << endl;
     }
   }
+
+  // another answer
+  // Aiのスコア値と順番をペアにしてソートするのでも良い
+  // int n;
+  // cin >> n;
+  // // Ai, 順番のpairを要素に持つ配列を用意
+  // vector<pair<int, int>> v;
+
+  // for (int i = 0; i < n; i++) {
+  //   int a;
+  //   cin >> a;
+  //   // make_pair()でpairを作成
+  //   v.push_back(make_pair(a, i + 1));
+  // }
+
+  // // pairを降順でソート(greater<pair<int, int>>)
+  // sort(v.begin(), v.end(), greater<pair<int, int>>());
+
+  // // 前から２番目（ブービー賞）の人の番号を表示
+  // cout << v[1].second << endl;
+
   return 0;
 }
