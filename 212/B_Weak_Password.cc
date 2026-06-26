afcc0e02b66872016a83f395ba220d087fd54e0d
ckp-koiken <kentaro.koi0710@gmail.com>
2026-06-26 12:45:04 +0900

    add another answer

--- 212/B_Weak_Password.cc
+++ 212/B_Weak_Password.cc
@@ -22,7 +22,22 @@ int main() {
     cout << "Weak" << endl;
     return 0;
   }
-
+  
   cout << "Strong" << endl;
+
+  // 4つ同じ数字かどうかはfor文でnextと一緒に確かめても良い
+  // bool same = true;
+  // for (int i = 0; i < 3; i++) {
+  //   if (X[i] != X[i + 1]) same = false;
+  //   if (((int)X[i] + 1) % 10 != (int)X[i + 1] % 10) {
+  //     next = false;
+  //   }
+  // }
+  // if (same || next) {
+  //   cout << "Weak" << endl;
+  // } else {
+  //   cout << "Strong" << endl;
+  // }
+
   return 0;
 }
