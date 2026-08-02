35ce89e437f57302cb96620331cdda759ca13a1b
ckp-koiken <kentaro.koi0710@gmail.com>
2026-08-02 17:17:50 +0900

    add another answer

--- 469/B_Isolated_Seats.cc
+++ 469/B_Isolated_Seats.cc
@@ -17,3 +17,23 @@ int main() {
   cout << N - cnt << endl;
   return 0;
 }
+
+// another answer
+// 問題文通りに書いても良い
+// int main() {
+//   int n;
+//   string s;
+//   cin >> n >> s;
+
+//   int ans = 0;
+
+//   for (int i = 0; i < n; i++) {
+//     bool empty = (s[i] == 'x');
+//     bool leftEmpty = (i == 0 || s[i - 1] == 'x');
+//     bool rightEmpty = (i == n - 1 || s[i + 1] == 'x');
+
+//     if (empty && leftEmpty && rightEmpty) ans++;
+//   }
+
+//   cout << ans << endl;
+// }
